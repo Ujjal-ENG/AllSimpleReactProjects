cda6ac5797f4a0dabf19d1a54f6a7debc5d7bb30
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-13 22:56:43 +0600

    done the register page functionality with design

--- email-password-auth/src/components/pages/Register.jsx
+++ email-password-auth/src/components/pages/Register.jsx
@@ -1,8 +1,86 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable indent */
-import React from 'react';
+/* eslint-disable comma-dangle */
+/* eslint-disable prettier/prettier */
+/* eslint-disable quotes */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable indent */
+
+import React, { useState } from "react";
 
 function Register() {
-    return <div>Register</div>;
+  const [formDetails, setFormDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formDetails;
+
+  const handleChange = (e) => {
+    setFormDetails({
+      ...formDetails,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formDetails);
+  };
+  return (
+    <div>
+      <h1 className="text-3xl text-violet-500 font-bold text-center py-10">
+        Register Page
+      </h1>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleSubmit}
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
