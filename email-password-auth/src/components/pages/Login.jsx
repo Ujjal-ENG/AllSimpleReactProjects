283030441e44464a31cf0aa6968d175a07850f22
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-14 01:07:02 +0600

    login and sign up seccesfuuly implemented

--- email-password-auth/src/components/pages/Login.jsx
+++ email-password-auth/src/components/pages/Login.jsx
@@ -8,15 +8,54 @@
 /* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable indent */
 
-import React from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import app from "../firebase/firebase.init";
 
 function Login() {
+  const [formDetails, setFormDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formDetails;
+
+  const handleChange = (e) => {
+    setFormDetails({
+      ...formDetails,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const auth = getAuth(app);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formDetails);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const { user } = userCredential;
+        navigate("/");
+        toast.success(`Login SuccessFull and welcome: ${user.displayName}`);
+        // ...
+      })
+      .catch((error) => {
+        toast.error(`Login SuccessFull and welcome: ${error}`);
+        console.log(error);
+      });
+  };
   return (
     <div>
       <h1 className="text-3xl text-violet-500 font-bold text-center py-10">
         Login Form
       </h1>
-      <form className="w-full max-w-md p-6 mx-auto rounded-lg shadow-md bg-gray-100">
+      <form
+        className="w-full max-w-md p-6 mx-auto rounded-lg shadow-md bg-gray-100"
+        onSubmit={handleSubmit}
+      >
         <h2 className="text-xl font-bold text-center mb-6">Sign In</h2>
         <div className="mb-6">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
@@ -27,6 +66,8 @@ function Login() {
             id="email"
             type="email"
             placeholder="email"
+            value={email}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-6">
@@ -41,12 +82,14 @@ function Login() {
             id="password"
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={handleChange}
           />
         </div>
         <div className="flex items-center justify-center">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
           >
             Sign In
           </button>
--- email-password-auth/src/main.jsx
+++ email-password-auth/src/main.jsx
@@ -3,10 +3,14 @@
 /* eslint-disable quotes */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./components/routes/App.Routes";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <>
+    <Toaster />
+    <RouterProvider router={router} />
+  </>
 );
