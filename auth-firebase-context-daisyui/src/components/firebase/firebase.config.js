9526f854b9ce935fe247fd3fd7ed1c05eedd3036
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-16 00:34:02 +0600

    added the nesseary package

---
+++ auth-firebase-context-daisyui/src/components/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAfCpfqHP80nNDLfSTZpBLEa_8sMj_mOw",
+  authDomain: "auth-firebase-context-44598.firebaseapp.com",
+  projectId: "auth-firebase-context-44598",
+  storageBucket: "auth-firebase-context-44598.appspot.com",
+  messagingSenderId: "700223511260",
+  appId: "1:700223511260:web:d0968abc8f58d34975f219"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
