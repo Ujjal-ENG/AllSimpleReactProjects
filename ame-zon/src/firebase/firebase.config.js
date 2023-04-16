d9dce0bec6af9c3ca3e24166c7e4d78945804199
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-16 22:49:14 +0600

    chnage some setting and added the firebase project setup

---
+++ ame-zon/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCnvDXYw5qcXU-PFZMaZifQadV8OxeSXqA',
+  authDomain: 'ame-zon.firebaseapp.com',
+  projectId: 'ame-zon',
+  storageBucket: 'ame-zon.appspot.com',
+  messagingSenderId: '225179330258',
+  appId: '1:225179330258:web:0c26d853f7807397ffaf0b',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
