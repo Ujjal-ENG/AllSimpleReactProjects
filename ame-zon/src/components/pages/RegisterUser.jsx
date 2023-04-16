6d72ae06ca1d514618a8f07f08e5c0926641f2b4
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-16 23:42:14 +0600

    create user account functionlity done, just not yet to add the google part

--- ame-zon/src/components/pages/RegisterUser.jsx
+++ ame-zon/src/components/pages/RegisterUser.jsx
@@ -1,12 +1,34 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable react/jsx-indent */
-import React from 'react';
+import React, { useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
 
 function RegisterUser() {
+    const [newUser, setNewUser] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+    const { name, email, password } = newUser;
+    const handleChange = (e) => {
+        setNewUser({
+            ...newUser,
+            [e.target.id]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(newUser);
+    };
+
+    const handleSignUpGoogle = async () => {
+        console.log('google');
+    };
     return (
         <div>
             <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -16,7 +38,7 @@ function RegisterUser() {
 
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                     <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                        <form className="space-y-6" method="POST">
+                        <form className="space-y-6" onSubmit={handleSubmit}>
                             <div>
                                 <label htmlFor="name" className="block text-sm font-medium text-gray-700">
                                     Name
@@ -26,6 +48,8 @@ function RegisterUser() {
                                         id="name"
                                         name="name"
                                         type="text"
+                                        value={name}
+                                        onChange={handleChange}
                                         autoComplete="name"
                                         required
                                         className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -42,6 +66,8 @@ function RegisterUser() {
                                         id="email"
                                         name="email"
                                         type="email"
+                                        value={email}
+                                        onChange={handleChange}
                                         autoComplete="email"
                                         required
                                         className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -58,22 +84,8 @@ function RegisterUser() {
                                         id="password"
                                         name="password"
                                         type="password"
-                                        autoComplete="new-password"
-                                        required
-                                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    />
-                                </div>
-                            </div>
-
-                            <div>
-                                <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">
-                                    Confirm Password
-                                </label>
-                                <div className="mt-1">
-                                    <input
-                                        id="confirm-password"
-                                        name="confirm-password"
-                                        type="password"
+                                        value={password}
+                                        onChange={handleChange}
                                         autoComplete="new-password"
                                         required
                                         className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -109,7 +121,8 @@ function RegisterUser() {
 
                                 <div className="mt-6 ">
                                     <button
-                                        type="submit"
+                                        type="button"
+                                        onClick={handleSignUpGoogle}
                                         className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 items-center gap-2">
                                         <FcGoogle className="text-3xl" />
                                         Register with Google
