8a8d30af65822296c5783c56b2d7306c560d7f83
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-16 22:43:44 +0600

    create sign in sign out and forget password pages

--- ame-zon/src/components/pages/Login.jsx
+++ ame-zon/src/components/pages/Login.jsx
@@ -1,55 +1,90 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="flex w-full justify-center items-center flex-col my-20">
-            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">SignUp</h2>
+        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+            </div>
 
-            <div className="relative mb-4">
-                <label htmlFor="name" className="leading-7 text-sm text-gray-600">
-                    Name
-                </label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form className="space-y-6" method="POST">
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                Email address
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                                Password
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center">
+                                <div className="text-sm">
+                                    <Link to="/register-user" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                        Are you New?
+                                    </Link>
+                                </div>
+                            </div>
+
+                            <div className="text-sm">
+                                <Link to="/forgot-password" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                    Forgot your password?
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Sign in
+                            </button>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                                Sign in with Google
+                            </button>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                                Sign in with Github
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="relative mb-4">
-                <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-                    Email
-                </label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-            </div>
-            <div className="relative mb-4">
-                <label htmlFor="message" className="leading-7 text-sm text-gray-600">
-                    Password
-                </label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-            </div>
-            <div className="relative mb-4">
-                <label htmlFor="message" className="leading-7 text-sm text-gray-600">
-                    Confirm Password
-                </label>
-                <input
-                    type="password"
-                    id="confirm-password"
-                    name="confirm-password"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-            </div>
-            <button className="text-white  bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Sign Up</button>
         </div>
     );
 };
--- ame-zon/src/components/routes/App.routes.jsx
+++ ame-zon/src/components/routes/App.routes.jsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../../App';
 import { cartProductsLoader } from '../../loaders/cartProductsLoaders';
 import ErrorPage from '../pages/ErrorPage';
+import ForgotPassword from '../pages/ForgotPassword';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import OrderReview from '../pages/OrderReview';
+import RegisterUser from '../pages/RegisterUser';
 import Shop from '../pages/Shop';
 export const router = createBrowserRouter([
     {
@@ -28,6 +30,14 @@ export const router = createBrowserRouter([
                 path: '/order-review',
                 element: <OrderReview />,
                 loader: cartProductsLoader
+            },
+            {
+                path: '/forgot-password',
+                element: <ForgotPassword />
+            },
+            {
+                path: '/register-user',
+                element: <RegisterUser />
             }
         ]
     }
