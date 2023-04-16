16fb3f5433f2d87a456b12032d9c24c6fbdde39c
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-16 23:31:02 +0600

    now the error page is most beautifull from before one

--- ame-zon/src/components/pages/ErrorPage.jsx
+++ ame-zon/src/components/pages/ErrorPage.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-indent */
-/* eslint-disable jsx-a11y/iframe-has-title */
+import { TbError404 } from 'react-icons/tb';
 import { useNavigate, useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
@@ -10,17 +10,22 @@ export default function ErrorPage() {
     };
 
     return (
-        <div id="error-page" className="h-screen w-full flex justify-center items-center space-y-4 flex-col">
-            <iframe src="https://embed.lottiefiles.com/animation/82815" className="h-[400px] w-[500px] rounded" />
-            <h1 className="text-4xl font-semibold text-orange-400">Oops!,Kire mama ei page, eta to nai..ager page ja</h1>
-            <p className="text-xl font-bold text-red-500">Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-
-            <button type="button" className="px-4 py-2 rounded-md bg-red-500 text-white w-1/2 font-bold" onClick={handlePage}>
-                Go Back to Previous Page!!
-            </button>
+        <div id="error-page" className="h-screen w-full flex justify-center items-center space-y-6 flex-col bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
+            <div className="w-full max-w-xl px-6 py-12 bg-white rounded-lg shadow-lg">
+                <div className="flex items-center justify-center mb-8">
+                    <TbError404 className="h-16 w-16 mr-4" />
+                    <h1 className="text-4xl font-bold text-gray-900">Oops, something went wrong!</h1>
+                </div>
+                <p className="text-xl font-medium text-gray-700">{error.statusText || error.message}</p>
+                <div className="mt-8">
+                    <button type="button" className="px-6 py-2 bg-red-600 text-white font-bold rounded-md mr-4" onClick={handlePage}>
+                        Go Back
+                    </button>
+                    <button type="button" className="px-6 py-2 bg-gray-400 text-white font-bold rounded-md" onClick={() => navigate('/')}>
+                        Go to Home
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
