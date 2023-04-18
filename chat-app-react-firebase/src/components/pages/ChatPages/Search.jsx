e7c51e6fe60e584ecabf56dd67490b0043a66f7f
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-19 01:52:30 +0600

    done the ide bar desing propely

--- chat-app-react-firebase/src/components/pages/ChatPages/Search.jsx
+++ chat-app-react-firebase/src/components/pages/ChatPages/Search.jsx
@@ -6,7 +6,7 @@ function Search() {
         <div className="bg-gray-600 h-fit">
             <input type="text" placeholder="Find a User" className="input  outline-none border-t-0 border-r-0 border-l-0 input-success w-full rounded-none border-2" />
 
-            <div className="flex items-start gap-2 p-2 my-3 border-b-2 border-b-red-400">
+            <div className="flex items-start gap-2 p-2  border-b-2 border-b-red-400 duration-150 cursor-pointer hover:bg-gray-700">
                 <div className="avatar online">
                     <div className="w-12 rounded-full">
                         <img src="https://images.pexels.com/photos/3790492/pexels-photo-3790492.jpeg?auto=compress&cs=tinysrgb&w=600" alt="avatar" />
@@ -17,7 +17,7 @@ function Search() {
                     <p className="text-xs text-white">Kopa mama kopa</p>
                 </div>
             </div>
-            <div className="flex items-start gap-2 p-2 my-3 border-b-2 border-b-red-400">
+            <div className="flex items-start gap-2 p-2 border-b-2 border-b-red-400 duration-150 cursor-pointer hover:bg-gray-700">
                 <div className="avatar online">
                     <div className="w-12 rounded-full">
                         <img src="https://images.pexels.com/photos/3790492/pexels-photo-3790492.jpeg?auto=compress&cs=tinysrgb&w=600" alt="avatar" />
