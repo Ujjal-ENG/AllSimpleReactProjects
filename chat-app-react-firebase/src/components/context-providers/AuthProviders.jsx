02c10b9e9edf326265f1dceefad1ba93c13ae01c
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-19 09:58:38 +0600

    set up the context

--- chat-app-react-firebase/src/components/context-providers/AuthProviders.jsx
+++ chat-app-react-firebase/src/components/context-providers/AuthProviders.jsx
@@ -1,8 +1,65 @@
-import React, { createContext } from 'react';
+/* eslint-disable object-curly-newline */
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable comma-dangle */
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../../firebase/firebase.config';
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext({});
+const auth = getAuth(app);
 function AuthProviders({ children }) {
-    return <div>{children}</div>;
+    const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState(null);
+    const [privateLoad, setPriveLoad] = useState(true);
+
+    const createUser = (email, password, name) => {
+        setLoading(true);
+        setPriveLoad(false);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const { user } = userCredential;
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                });
+                setLoading(false);
+
+                // ...
+            })
+            .catch((error) => {
+                // ..
+            });
+    };
+
+    const signINUser = (email, password) => {
+        setLoading(true);
+        setPriveLoad(false);
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const { user } = userCredential;
+                // ...
+                setLoading(false);
+            })
+            .catch((error) => {});
+    };
+
+    useEffect(() => {
+        const subscriber = onAuthStateChanged(auth, (currentUser) => {
+            setPriveLoad(false);
+            setUser(currentUser);
+        });
+        return () => subscriber();
+    }, [user]);
+
+    const auths = {
+        loading,
+        privateLoad,
+        createUser,
+        signINUser,
+        user
+    };
+    return <AuthContext.Provider value={auths}>{children}</AuthContext.Provider>;
 }
 
 export default AuthProviders;
