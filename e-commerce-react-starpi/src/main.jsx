9a0a1071e79213e05e0fe12d14208e6ee71aa4d0
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-20 22:38:59 +0600

    not done properly all

--- e-commerce-react-starpi/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './components/routes/App.Routes';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
