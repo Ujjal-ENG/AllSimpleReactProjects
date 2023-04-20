3e7f9a61326549d9978ba169161bc5e41754bd5a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-20 20:05:42 +0600

    i think the design is done navbar and footer

--- e-commerce-react-starpi/src/App.jsx
+++ e-commerce-react-starpi/src/App.jsx
@@ -1,5 +1,18 @@
+/* eslint-disable react/jsx-indent */
+import { Outlet } from 'react-router-dom';
+import { Footer } from './components/layouts/Footer';
+import Navbar from './components/layouts/Navbar';
+
 function App() {
-    return <></>;
+    return (
+        <>
+            <Navbar />
+            <div className="min-h-[calc(100vh-264px)]">
+                <Outlet />
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- e-commerce-react-starpi/src/components/layouts/Navbar.jsx
+++ e-commerce-react-starpi/src/components/layouts/Navbar.jsx
@@ -1,6 +1,52 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
-const Navbar = () => {
-    return <></>;
-};
+import { AiOutlineSearch } from 'react-icons/ai';
+import { GiSelfLove } from 'react-icons/gi';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+import { MdSelfImprovement } from 'react-icons/md';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav className="flex items-center shadow-2xl py-4 justify-between px-4 ">
+            <section className="left space-x-4">
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>Country</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>USD</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>Men</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>Women</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>Children</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>Accessories</NavLink>
+            </section>
+            <section className="middle">
+                <h2 className="font-bold text-2xl">E-Com-Roy</h2>
+            </section>
+            <section className="right space-x-4 flex items-center">
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>HomePage</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>About</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>Contact</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>Stores</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                    {' '}
+                    <AiOutlineSearch className="text-2xl" />
+                </NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                    {' '}
+                    <MdSelfImprovement className="text-2xl" />
+                </NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                    {' '}
+                    <GiSelfLove className="text-2xl" />
+                </NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                    {' '}
+                    <div className="relative">
+                        <HiOutlineShoppingCart className="text-2xl" />
+                        <span className="absolute -top-3 -right-2 w-5 h-5 text-center inset-2 rounded-full bg-blue-700 text-white">0</span>
+                    </div>
+                </NavLink>
+            </section>
+        </nav>
+    );
+}
 
 export default Navbar;
