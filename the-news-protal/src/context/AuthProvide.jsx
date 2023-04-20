ecac50bd8786cd2af76081fadf8c80f6437491f6
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-21 03:31:03 +0600

    change the useContext

--- the-news-protal/src/context/AuthProvide.jsx
+++ the-news-protal/src/context/AuthProvide.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React, { createContext } from 'react';
 
-export const AuthContext = createContext();
+export const AuthContext = createContext(null);
 function AuthProvide({ children }) {
     const a = 2;
     return <AuthContext.Provider value={a}> {children}</AuthContext.Provider>;
