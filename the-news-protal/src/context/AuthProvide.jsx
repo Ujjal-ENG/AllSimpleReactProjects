48510b043e27f491e30ca2a08b7afaf86aac6135
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-21 03:30:31 +0600

    created the context and provided the all the child in globally and refused the prop drillign problem n react

---
+++ the-news-protal/src/context/AuthProvide.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext();
+function AuthProvide({ children }) {
+    const a = 2;
+    return <AuthContext.Provider value={a}> {children}</AuthContext.Provider>;
+}
+
+export default AuthProvide;
--- the-news-protal/src/main.jsx
+++ the-news-protal/src/main.jsx
@@ -1,7 +1,14 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './components/routes/App.Routes';
+import AuthProvide from './context/AuthProvide';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <AuthProvide>
+        <RouterProvider router={router} />
+    </AuthProvide>
+);
