711033ccce71b3543c29525dde520d46482975d5
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-20 23:01:03 +0600

    done the insital loading

---
+++ the-news-protal/src/components/layouts/leftsite/LeftNavCategoriresData.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LeftNavCategoriresData() {
+    return <div>LeftNavCategoriresData</div>;
+}
+
+export default LeftNavCategoriresData;
--- the-news-protal/src/components/routes/App.Routes.jsx
+++ the-news-protal/src/components/routes/App.Routes.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable comma-dangle */
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../../App';
+import LeftNavCategoriresData from '../layouts/leftsite/LeftNavCategoriresData';
 import Home from '../pages/Home';
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home />
+            },
+            {
+                path: '/category/:id',
+                element: <LeftNavCategoriresData />
             }
         ]
     }
