e4d74c4f9a019458e9c86ba170e3f3c20e9b677b
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-21 02:23:20 +0600

    based on the data shown the error message

--- the-news-protal/src/components/layouts/main-part/EachNewsCard.jsx
+++ the-news-protal/src/components/layouts/main-part/EachNewsCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable comma-dangle */
@@ -15,52 +16,78 @@ function EachNewsCard({ data }) {
     const location = useLocation();
     const news = data;
     return (
-        <div className="h-screen overflow-y-scroll scrollbar-hidden">
-            {news &&
-                news.map((el) => (
-                    <div className="border-2 rounded-xl my-4" key={el._id}>
-                        <div className="flex justify-start items-start gap-2 rounded-t-md bg-gray-200 p-4">
-                            <div className="avatar placeholder">
-                                <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
-                                    <img src={el.author.img} alt={el.author.name} />
+        <>
+            {news.length === 0 ? (
+                <div className="alert alert-warning shadow-lg">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                            />
+                        </svg>
+                        <span>Warning: {news.length} data found!!!</span>
+                    </div>
+                </div>
+            ) : (
+                <div className="alert alert-success shadow-lg">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <span>{news.length} data found!!!</span>
+                    </div>
+                </div>
+            )}
+            <div className="h-screen overflow-y-scroll scrollbar-hidden">
+                {news &&
+                    news.map((el) => (
+                        <div className="border-2 rounded-xl my-4" key={el._id}>
+                            <div className="flex justify-start items-start gap-2 rounded-t-md bg-gray-200 p-4">
+                                <div className="avatar placeholder">
+                                    <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
+                                        <img src={el.author.img} alt={el.author.name} />
+                                    </div>
+                                </div>
+
+                                <div className="grow justify-start flex-col">
+                                    <h3 className="text-xl font-bold">{el.author.name}</h3>
+                                    <h3 className="text-gray-400">{moment().format('yyy Mo ddd')}</h3>
+                                </div>
+
+                                <div className="grow-0 flex justify-between items-center gap-4 text-3xl">
+                                    <BsBookmark />
+                                    <BsShare />
                                 </div>
                             </div>
 
-                            <div className="grow justify-start flex-col">
-                                <h3 className="text-xl font-bold">{el.author.name}</h3>
-                                <h3 className="text-gray-400">{moment().format('yyy Mo ddd')}</h3>
-                            </div>
+                            <h1 className="text-2xl font-bold p-4">{el.title}</h1>
 
-                            <div className="grow-0 flex justify-between items-center gap-4 text-3xl">
-                                <BsBookmark />
-                                <BsShare />
-                            </div>
-                        </div>
+                            <img src={el.image_url} alt="" className="mx-auto object-cover p-4" />
 
-                        <h1 className="text-2xl font-bold p-4">{el.title}</h1>
+                            <p className="text-gray-400 tracking-widest px-4">{`${el.details.slice(0, 200)}...`}</p>
+                            <Link to={`/news/${el._id}`} state={location} type="button" className="btn btn-ghost text-yellow-400 font-bold mx-2">
+                                Read More
+                            </Link>
 
-                        <img src={el.image_url} alt="" className="mx-auto object-cover p-4" />
+                            <div className="pt-4 border-t-2 mx-5" />
 
-                        <p className="text-gray-400 tracking-widest px-4">{`${el.details.slice(0, 200)}...`}</p>
-                        <Link to={`/news/${el._id}`} state={location} type="button" className="btn btn-ghost text-yellow-400 font-bold mx-2">
-                            Read More
-                        </Link>
-
-                        <div className="pt-4 border-t-2 mx-5" />
-
-                        <div className="p-4 flex justify-between items-center">
-                            <div className="flex items-center gap-2">
-                                <BsFillStarFill className="text-yellow-500" />
-                                <p>{el.rating.number}</p>
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <BsFillEyeFill />
-                                <p>{el.total_view}</p>
+                            <div className="p-4 flex justify-between items-center">
+                                <div className="flex items-center gap-2">
+                                    <BsFillStarFill className="text-yellow-500" />
+                                    <p>{el.rating.number}</p>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                    <BsFillEyeFill />
+                                    <p>{el.total_view}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-        </div>
+                    ))}
+            </div>
+        </>
     );
 }
 
