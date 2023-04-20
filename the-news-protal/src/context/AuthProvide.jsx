d5986e2a5f5e0078138baa696372873f900443fe
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-21 03:40:26 +0600

    singin method created

--- the-news-protal/src/context/AuthProvide.jsx
+++ the-news-protal/src/context/AuthProvide.jsx
@@ -1,10 +1,70 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable comma-dangle */
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
 function AuthProvide({ children }) {
-    const a = 2;
-    return <AuthContext.Provider value={a}> {children}</AuthContext.Provider>;
+    const [loading, setLoading] = useState(false);
+    const [privateLoad, setPrivateLoad] = useState(true);
+    const [userInfo, setUserInfo] = useState(null);
+
+    const createUser = async (email, password, name) => {
+        setLoading(true);
+        setLoading(true);
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            await updateProfile(user, {
+                displayName: name
+            });
+            setLoading(false);
+            console.log(user);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const signInUser = async (email, password) => {
+        setLoading(true);
+        setLoading(true);
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, password);
+            setLoading(false);
+            console.log(user);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        const unsubscirber = onAuthStateChanged(auth, (user) => {
+            setLoading(false);
+            setPrivateLoad(false);
+            setUserInfo(user);
+        });
+        return () => unsubscirber();
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="h-screen flex justify-center items-center">
+                <progress className="progress w-56" />
+            </div>
+        );
+    }
+
+    const auths = {
+        privateLoad,
+        createUser,
+        signInUser,
+        userInfo
+    };
+
+    return <AuthContext.Provider value={auths}> {children}</AuthContext.Provider>;
 }
 
 export default AuthProvide;
