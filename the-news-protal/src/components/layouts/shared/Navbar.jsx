57a4c95c2a8f5b80974f3d9772505c1bbf1e7460
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-21 20:35:08 +0600

    make the navbar responsive

--- the-news-protal/src/components/layouts/shared/Navbar.jsx
+++ the-news-protal/src/components/layouts/shared/Navbar.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-indent */
 import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
@@ -10,35 +9,33 @@ function Navbar() {
         singOutUser();
     };
     return (
-        <nav className="px-4 py-3 shadow-lg my-5 max-w-7xl mx-auto">
-            <div className="flex justify-between">
-                <div className="flex items-center text-white">
-                    <span className="font-semibold text-xl tracking-tight" />
+        <nav className="px-4 py-3 shadow-lg my-5 max-w-7xl mx-auto flex flex-wrap md:justify-between md:items-center">
+            <div className="flex items-center text-white mb-4 md:mb-0">
+                <span className="font-semibold text-xl tracking-tight" />
+            </div>
+            <div className="flex-1 flex justify-center">
+                <div className="text-2xl flex justify-center items-center gap-4">
+                    <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                        Home
+                    </NavLink>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                        About
+                    </NavLink>
+                    <NavLink to="/career" className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                        Career
+                    </NavLink>
                 </div>
-                <div className="flex-1 flex justify-center">
-                    <div className="text-2xl flex justify-center items-center gap-4">
-                        <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'default')}>
-                            Home
-                        </NavLink>
-                        <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'default')}>
-                            About
-                        </NavLink>
-                        <NavLink to="/career" className={({ isActive }) => (isActive ? 'active' : 'default')}>
-                            Career
-                        </NavLink>
+            </div>
+            <div className="flex justify-between items-center gap-3">
+                <div className="avatar">
+                    <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={userInfo ? userInfo.photoURL : 'https://images.pexels.com/photos/2253415/pexels-photo-2253415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} alt="avatar" />
                     </div>
                 </div>
-                <div className="flex justify-between items-center gap-3">
-                    <div className="avatar">
-                        <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                            <img src={userInfo ? userInfo.photoURL : 'https://images.pexels.com/photos/2253415/pexels-photo-2253415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} alt="avatar" />
-                        </div>
-                    </div>
-                    <h3 className="text-2xl font-bold">{userInfo && userInfo.displayName}</h3>
-                    <Link to={!userInfo && '/login'} onClick={userInfo && handleSignout} className="btn btn-md text-white font-bold btn-ghost bg-gray-700 hover:text-gray-700">
-                        {userInfo ? 'Logout' : 'Login'}
-                    </Link>
-                </div>
+                <h3 className="text-2xl font-bold">{userInfo && userInfo.displayName}</h3>
+                <Link to={!userInfo && '/login'} onClick={userInfo && handleSignout} className="btn btn-md text-white font-bold btn-ghost bg-gray-700 hover:text-gray-700">
+                    {userInfo ? 'Logout' : 'Login'}
+                </Link>
             </div>
         </nav>
     );
