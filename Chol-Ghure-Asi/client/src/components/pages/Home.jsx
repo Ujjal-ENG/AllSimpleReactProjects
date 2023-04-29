a32ccf209de290e076faf822291545193f59115c
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-30 04:13:49 +0600

    added the video and run script

--- Chol-Ghure-Asi/client/src/components/pages/Home.jsx
+++ Chol-Ghure-Asi/client/src/components/pages/Home.jsx
@@ -1,7 +1,18 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import BackgroundVideo from '../../assets/video/video.mp4';
 
 function Home() {
-    return <h1>hekkko</h1>;
+    return (
+        <div>
+            <video autoPlay muted loop className="fixed top-0 left-0 w-full h-full object-cover z-0">
+                <source src={BackgroundVideo} type="video/mp4" />
+            </video>
+            <div className="relative z-10">
+                <h1>Kopa Mam</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
