206f86027846a109acdce9e4df4888317b975b7e
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 00:26:44 +0600

    setup is already done now time to create data

--- Chol-Ghure-Asi/client/src/components/pages/RegisterPage.jsx
+++ Chol-Ghure-Asi/client/src/components/pages/RegisterPage.jsx
@@ -1,13 +1,36 @@
+/* eslint-disable consistent-return */
+/* eslint-disable comma-dangle */
 /* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import { FaFacebook, FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Navbar from '../layouts/Navbar';
 
 function RegisterPage() {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData((ps) => ({
+            ...ps,
+            [e.target.id]: e.target.value
+        }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (formData.password !== formData.confirmPassword) {
+            return toast.error('Please Provide the correct password!!!');
+        }
+        console.log(formData);
+    };
     return (
         <>
             <div className="bg-black">
@@ -18,7 +41,7 @@ function RegisterPage() {
                     <div>
                         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create an account</h2>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
+                    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
                         <input type="hidden" name="remember" value="true" />
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
@@ -29,6 +52,8 @@ function RegisterPage() {
                                     id="username"
                                     name="username"
                                     type="text"
+                                    value={formData.username}
+                                    onChange={handleChange}
                                     autoComplete="username"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
@@ -36,13 +61,15 @@ function RegisterPage() {
                                 />
                             </div>
                             <div>
-                                <label htmlFor="email-address" className="sr-only">
+                                <label htmlFor="email" className="sr-only">
                                     Email address
                                 </label>
                                 <input
-                                    id="email-address"
+                                    id="email"
                                     name="email"
                                     type="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
                                     autoComplete="email"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
@@ -57,6 +84,8 @@ function RegisterPage() {
                                     id="password"
                                     name="password"
                                     type="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
                                     autoComplete="current-password"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
@@ -64,13 +93,15 @@ function RegisterPage() {
                                 />
                             </div>
                             <div>
-                                <label htmlFor="confirm-password" className="sr-only">
+                                <label htmlFor="confirPassword" className="sr-only">
                                     Confirm Password
                                 </label>
                                 <input
-                                    id="confirm-password"
-                                    name="confirm-password"
+                                    id="confirmPassword"
+                                    name="confirmPassword"
                                     type="password"
+                                    value={formData.confirmPassword}
+                                    onChange={handleChange}
                                     autoComplete="current-password"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
