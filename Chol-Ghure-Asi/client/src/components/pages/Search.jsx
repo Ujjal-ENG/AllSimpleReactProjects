0a0537e8deb317186fe851781829f4be513f3520
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 01:51:13 +0600

    now i will make the serarchg backend

--- Chol-Ghure-Asi/client/src/components/pages/Search.jsx
+++ Chol-Ghure-Asi/client/src/components/pages/Search.jsx
@@ -1,7 +1,19 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import Navbar from '../layouts/Navbar';
 
 function Search() {
-    return <div>Search</div>;
+    return (
+        <div>
+            <div className="bg-black">
+                <Navbar />
+            </div>
+        </div>
+    );
 }
 
 export default Search;
