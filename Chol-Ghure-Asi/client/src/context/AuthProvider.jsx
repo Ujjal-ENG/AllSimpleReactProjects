36d6cf407a65b562868a5b014c2b2a39862371dc
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-30 23:22:11 +0600

    added this react hot toast

--- Chol-Ghure-Asi/client/src/context/AuthProvider.jsx
+++ Chol-Ghure-Asi/client/src/context/AuthProvider.jsx
@@ -1,8 +1,15 @@
-import React, { createContext } from 'react';
+import { getAuth } from 'firebase/auth';
+import React, { createContext, useState } from 'react';
+import app from '../config/firebse';
 
 export const AuthContext = createContext(null);
 
+const auth = getAuth(app);
+
 function AuthProvider({ children }) {
+    const [loading, setIsLoading] = useState(false);
+    const [privateLoad, setPrivateLoad] = useState(true);
+    const [userInfo, setUserInfo] = useState(null);
     return <AuthContext.Provider value={4}>{children}</AuthContext.Provider>;
 }
 
