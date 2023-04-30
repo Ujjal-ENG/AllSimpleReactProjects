93687af62e6bbb791feb3348920f57d633a1255c
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-30 21:16:24 +0600

    now work to the another work

--- Chol-Ghure-Asi/client/src/components/pages/SingleBooking.jsx
+++ Chol-Ghure-Asi/client/src/components/pages/SingleBooking.jsx
@@ -1,7 +1,31 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Navbar from '../layouts/Navbar';
+import LeftSide from './LeftSide';
 
 function SingleBooking() {
-    return <div>SingleBooking</div>;
+    const loader = useLoaderData();
+
+    return (
+        <div>
+            <div className=" fixed top-0 left-0 w-full h-full bg-cover bg-center z-0 duration-200 transition-all ease-in" style={{ backgroundImage: `url(${loader.findData.image})` }} />
+
+            <div className="absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-30" />
+            <div className="relative z-10">
+                <Navbar />
+
+                <div className="grid grid-cols-12 justify-items-center px-12 items-center h-full mt-12">
+                    <div className="col-span-3">
+                        <LeftSide data={loader.findData} id={loader.findData.id} />
+                    </div>
+                    <div className="col-span-9 ml-24">
+                        <h1>Hello</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default SingleBooking;
