bbcf90445aed0614bde627c0d5c61ea515e98dfb
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 04:00:02 +0600

    added secret key more usablity

--- Chol-Ghure-Asi/client/src/config/firebse.js
+++ Chol-Ghure-Asi/client/src/config/firebse.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDOdW7EKNXhLLNvMNk_rPdJ7ijgFn0wmcI',
-  authDomain: 'chol-ghure-asa-jak.firebaseapp.com',
-  projectId: 'chol-ghure-asa-jak',
-  storageBucket: 'chol-ghure-asa-jak.appspot.com',
-  messagingSenderId: '333644533926',
-  appId: '1:333644533926:web:a4bdc4de7e26a56cc7e467',
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
