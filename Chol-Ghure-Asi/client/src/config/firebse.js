19adfb272a959e705e24fb1d19c30e6707c2b91d
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-30 23:18:30 +0600

    done the firebase config

---
+++ Chol-Ghure-Asi/client/src/config/firebse.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCUjOr6dZABKf9dJI8eKAoNnhNpbzpGx8s',
+  authDomain: 'chol-ghure-asi.firebaseapp.com',
+  projectId: 'chol-ghure-asi',
+  storageBucket: 'chol-ghure-asi.appspot.com',
+  messagingSenderId: '912444847687',
+  appId: '1:912444847687:web:99a6d3f4c8647e957d2362',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
