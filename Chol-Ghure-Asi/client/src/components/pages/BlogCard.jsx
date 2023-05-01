57fcc2ab26f2b5519d22c78c2628f1bb0bba18ea
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 18:19:07 +0600

    for showing blog maked the blog card

---
+++ Chol-Ghure-Asi/client/src/components/pages/BlogCard.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+import { FaClock, FaUser } from 'react-icons/fa';
+
+function BlogCard({ data }) {
+    const { image, title, description, author, date } = data;
+    return (
+        <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            <div className="md:flex">
+                <div className="md:flex-shrink-0">
+                    <img className="h-48 w-full object-cover md:w-48" src={image} alt="" />
+                </div>
+                <div className="p-8">
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{date}</div>
+                    <h2 className="block mt-1 text-lg leading-tight font-medium text-black">{title}</h2>
+                    <p className="mt-2 text-gray-500">{description}</p>
+                    <div className="mt-4 flex items-center">
+                        <FaUser className="mr-2" />
+                        <div className="text-sm font-medium text-gray-900">{author}</div>
+                        <FaClock className="ml-auto animate-bounce" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BlogCard;
