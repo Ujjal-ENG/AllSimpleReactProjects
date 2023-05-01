9313fcbc73804d2a4e34fd5ec81e966bd3ef9a66
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 18:20:59 +0600

    some design change in Blogs page

--- Chol-Ghure-Asi/client/src/components/pages/Blog.jsx
+++ Chol-Ghure-Asi/client/src/components/pages/Blog.jsx
@@ -1,7 +1,19 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import Navbar from '../layouts/Navbar';
 
 function Blog() {
-    return <div>Blog</div>;
+    return (
+        <div>
+            <div className="bg-black">
+                <Navbar />
+            </div>
+
+            <h1 className="hover-stroke text-5xl font-bold" data-text="Blogs">
+                Blogs
+            </h1>
+        </div>
+    );
 }
 
 export default Blog;
