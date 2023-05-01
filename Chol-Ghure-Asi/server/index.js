372ec96afac38dbb124f94bed3182aa01529c18c
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 16:50:03 +0600

    done the route createing also the backend is goind well

--- Chol-Ghure-Asi/server/index.js
+++ Chol-Ghure-Asi/server/index.js
@@ -2,12 +2,18 @@ const cors = require('cors');
 const express = require('express');
 const data = require('./data/travelSite.json');
 const roomData = require('./data/roomdata.json');
+const destinationData = require('./data/destination.json');
 
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// default route
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello Ujjal From Backend and your server is well!!' });
+});
 
 // routes
 app.get('/all-travel-data', (req, res) => {
@@ -36,6 +42,15 @@ app.get('/search-room', (req, res) => {
         message: 'Succesffully get the Data',
         results: roomData.room.length,
         roomData: roomData.room,
+    });
+});
+
+app.get('/travel-destination', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'Succesffully get the Destination Data!!',
+        results: destinationData.destination.length,
+        roomData: destinationData.destination,
     });
 });
 
