5ee0000c3b247c900bb4c34c60b9c469bd039d7e
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-08 16:04:34 +0600

    done the card desing

--- coffee-expresso/client/src/components/pages/Home.jsx
+++ coffee-expresso/client/src/components/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import HeaderCard from './HeaderCard';
+import OurPopularProducts from './OurPopularProducts';
 
 const Home = () => {
     return (
         <div>
             <Header />
             <HeaderCard />
+            <div className="py-4" />
+            <OurPopularProducts />
         </div>
     );
 };
---
+++ coffee-expresso/client/src/components/pages/OurPopularProducts.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import { AiFillDelete, AiFillEye } from 'react-icons/ai';
+import { FaCoffee, FaEdit } from 'react-icons/fa';
+import Cup from '../../assets/cup.png';
+import SHop from '../../assets/shop.png';
+
+const OurPopularProducts = () => {
+    return (
+        <div className="relative">
+            <img src={Cup} alt="cup" className="absolute z-20 top-20" />
+            <img src={SHop} alt="shop" className="absolute  top-20 right-0" />
+
+            <h4 className="text-2xl text-center py-2"> --- Sip & Savor ---</h4>
+            <h1 className="text-5xl tracking-wider text-center py-2 font-semibold">Our Popular Products</h1>
+            <div className="w-full flex items-center justify-center py-5 ">
+                <button type="button" className="btn text-2xl font-bold flex items-center gap-3 btn-primary bg-[#E3B577]">
+                    Add Coffee
+                    <FaCoffee className="text-3xl font-bold text-black" />
+                </button>
+            </div>
+
+            {/* coffee cars */}
+            <div className="grid grid-cols-2 justify-items-center mt-16 my-container">
+                <div className="card card-side bg-base-100 max-w-3xl w-full duration-200 transition-all ease-in-out shadow-xl hover:shadow-2xl hover:translate-y-2">
+                    <figure>
+                        <img src="https://images.pexels.com/photos/2648988/pexels-photo-2648988.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Movie" className="w-56 h-48 object-cover" />
+                    </figure>
+                    <div className="flex justify-between items-center">
+                        <div className=" mx-2 space-y-2">
+                            <h2 className="card-title">
+                                Name: <span className="font-normal text-md">Americano Coffee</span>
+                            </h2>
+                            <h2 className="card-title">
+                                Chef: <span className="font-normal text-md">Ujjal Roy</span>
+                            </h2>
+                            <h2 className="card-title">
+                                Price: <span className="font-normal text-md">$80</span>
+                            </h2>
+                        </div>
+                        <div className="card-actions flex justify-end items-end border-2 relative">
+                            <div className="btn-group btn-group-vertical space-y-1 absolute -right-28 -bottom-20">
+                                <button type="button" className="bg-[#E3B577] p-2 rounded-md ">
+                                    <AiFillEye className="text-4xl  text-white cursor-pointer mx-auto" />
+                                </button>
+                                <button type="button" className="bg-black p-2 rounded-md">
+                                    <FaEdit className="text-4xl  text-white cursor-pointer mx-auto" />
+                                </button>
+                                <button type="button" className="bg-red-400 p-2 rounded-md">
+                                    <AiFillDelete className="text-4xl  text-white cursor-pointer mx-auto" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OurPopularProducts;
