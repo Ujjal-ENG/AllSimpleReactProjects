8de9ca0da51fbcddb650ce0736177560e9a4ad7e
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-08 16:24:56 +0600

    setup the routes

--- coffee-expresso/client/src/routes/App.Routes.jsx
+++ coffee-expresso/client/src/routes/App.Routes.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable import/extensions */
 /* eslint-disable comma-dangle */
 /* eslint-disable import/prefer-default-export */
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import AddToCoffe from '../components/pages/ButtonPage/AddToCoffe';
+import CoffeeDeatils from '../components/pages/ButtonPage/CoffeeDeatils';
+import UpdateCoffeDetails from '../components/pages/ButtonPage/UpdateCoffeDetails.jsx';
 import Home from '../components/pages/Home';
 
 export const router = createBrowserRouter([
@@ -12,6 +16,18 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home />
+            },
+            {
+                path: 'add-coffee',
+                element: <AddToCoffe />
+            },
+            {
+                path: 'update-coffee',
+                element: <UpdateCoffeDetails />
+            },
+            {
+                path: 'view-coffee',
+                element: <CoffeeDeatils />
             }
         ]
     }
