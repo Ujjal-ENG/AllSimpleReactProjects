26ab1d638b12e2a05400564a1094f3ee3d0a2ec0
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-08 18:53:07 +0600

    Fetch data complete

--- coffee-expresso/client/src/components/pages/OurPopularProducts.jsx
+++ coffee-expresso/client/src/components/pages/OurPopularProducts.jsx
@@ -1,12 +1,30 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react';
-import { AiFillDelete, AiFillEye } from 'react-icons/ai';
-import { FaCoffee, FaEdit } from 'react-icons/fa';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { FaCoffee } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Cup from '../../assets/cup.png';
 import SHop from '../../assets/shop.png';
+import PopularProduct from './PopularProduct';
 
 const OurPopularProducts = () => {
+    const [coffees, setCoffees] = useState([]);
+    // get all coffee data
+    const getAllCoffeeData = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:8080/all-coffees');
+            if (data.success) {
+                setCoffees(data.allCoffee);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getAllCoffeeData();
+    }, [coffees]);
     return (
         <div className="relative">
             <img src={Cup} alt="cup" className="absolute z-20 top-20" />
@@ -22,39 +40,8 @@ const OurPopularProducts = () => {
             </div>
 
             {/* coffee cars */}
-            <div className="grid grid-cols-2 justify-items-center mt-16 my-container">
-                <div className="card card-side bg-gray-200 max-w-3xl w-full duration-200 transition-all ease-in-out shadow-xl hover:shadow-2xl hover:translate-y-2">
-                    <figure>
-                        <img src="https://images.pexels.com/photos/2648988/pexels-photo-2648988.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Movie" className="w-60 h-52 object-cover p-8" />
-                    </figure>
-                    <div className="flex justify-between items-center">
-                        <div className=" mx-2 space-y-2">
-                            <h2 className="card-title">
-                                Name: <span className="font-normal text-md">Americano Coffee</span>
-                            </h2>
-                            <h2 className="card-title">
-                                Chef: <span className="font-normal text-md">Ujjal Roy</span>
-                            </h2>
-                            <h2 className="card-title">
-                                Price: <span className="font-normal text-md">$80</span>
-                            </h2>
-                        </div>
-                        <div className="card-actions flex justify-end items-end border-2 relative">
-                            <div className="btn-group btn-group-vertical space-y-1 absolute -right-28 -bottom-20">
-                                <Link to="/view-coffee" type="button" className="bg-[#E3B577] p-2 rounded-md ">
-                                    <AiFillEye className="text-4xl  text-white cursor-pointer mx-auto" />
-                                </Link>
-                                <Link to="/update-coffee" type="button" className="bg-black p-2 rounded-md">
-                                    <FaEdit className="text-4xl  text-white cursor-pointer mx-auto" />
-                                </Link>
-                                <button type="button" className="bg-red-400 p-2 rounded-md">
-                                    <AiFillDelete className="text-4xl  text-white cursor-pointer mx-auto" />
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+            <div className="grid grid-cols-2 gap-6 justify-items-center mt-16 my-container">{coffees && coffees.map((coffee) => <PopularProduct key={coffee._id} data={coffee} />)}</div>
         </div>
     );
 };
--- coffee-expresso/client/src/components/pages/PopularProduct.jsx
+++ coffee-expresso/client/src/components/pages/PopularProduct.jsx
@@ -1,7 +1,44 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+import { AiFillDelete, AiFillEye } from 'react-icons/ai';
+import { FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-const PopularProduct = () => {
-    return <div>PopularProduct</div>;
+const PopularProduct = ({ data }) => {
+    const { name, chef, photo } = data;
+    return (
+        <div className="card card-side bg-gray-200 max-w-3xl w-full duration-200 transition-all ease-in-out shadow-xl hover:shadow-2xl hover:translate-y-2">
+            <figure>
+                <img src={photo} alt={name} className="w-60 h-52 object-cover p-8" />
+            </figure>
+            <div className="flex justify-between items-center">
+                <div className=" mx-2 space-y-2 w-[220px]">
+                    <h2 className="card-title flex items-center">
+                        Name: <span className="font-normal tracking-tighter text-md">{name}</span>
+                    </h2>
+                    <h2 className="card-title">
+                        Chef: <span className="font-normal  text-md">{chef}</span>
+                    </h2>
+                    <h2 className="card-title">
+                        Price: <span className="font-normal text-md">$80</span>
+                    </h2>
+                </div>
+                <div className="card-actions flex justify-end items-end border-2 relative">
+                    <div className="btn-group btn-group-vertical space-y-1 absolute -right-28 -bottom-20">
+                        <Link to="/view-coffee" type="button" className="bg-[#E3B577] p-2 rounded-md ">
+                            <AiFillEye className="text-4xl  text-white cursor-pointer mx-auto" />
+                        </Link>
+                        <Link to="/update-coffee" type="button" className="bg-black p-2 rounded-md">
+                            <FaEdit className="text-4xl  text-white cursor-pointer mx-auto" />
+                        </Link>
+                        <button type="button" className="bg-red-400 p-2 rounded-md">
+                            <AiFillDelete className="text-4xl  text-white cursor-pointer mx-auto" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PopularProduct;
