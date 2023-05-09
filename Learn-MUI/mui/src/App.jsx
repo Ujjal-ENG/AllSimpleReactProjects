535320a3276c2d1092a93f970aa464ab9fd27add
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-09 18:55:31 +0600

    Added darkmode and light mode

--- Learn-MUI/mui/src/App.jsx
+++ Learn-MUI/mui/src/App.jsx
@@ -1,63 +1,41 @@
-/* eslint-disable react/jsx-indent-props */
 /* eslint-disable max-len */
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-closing-bracket-location */
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
 import * as React from 'react';
-import Home from './components/pages/Home';
+import { ColorModeContext } from './context/DarkModeProvider';
 
-const App = () => {
+function App() {
+    const theme = useTheme();
+    const colorMode = React.useContext(ColorModeContext);
     return (
-        <>
-            <Home />
-            <div className="flex flex-col gap-5 md:flex-row items-center justify-evenly mt-10">
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        alt="green iguana"
-                        image="https://images.pexels.com/photos/9139846/pexels-photo-9139846.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-                        className="h-60"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Lizard
-                        </Typography>
-                        <Typography variant="p" className="text-red-500 font-bold">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Share</Button>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                </Card>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        alt="green iguana"
-                        height="140"
-                        image="https://images.pexels.com/photos/16148497/pexels-photo-16148497.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-                        className="h-60 object-fill"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Lizard
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Share</Button>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                </Card>
-            </div>
-        </>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'end',
+                bgcolor: 'background.default',
+                color: 'text.primary',
+                borderRadius: 1
+            }}>
+            {theme.palette.mode}
+            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+                {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+            <Typography variant="h3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, deleniti ipsa, inventore incidunt, vel fugit voluptatum repellat at nostrum corrupti commodi quis! Dolor voluptatibus
+                minima quo totam numquam id ut!
+            </Typography>
+        </Box>
     );
-};
+}
 
 export default App;
--- Learn-MUI/mui/src/main.jsx
+++ Learn-MUI/mui/src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ToggleColorMode from './context/DarkModeProvider';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <ToggleColorMode>
+        <App />
+    </ToggleColorMode>
+);
