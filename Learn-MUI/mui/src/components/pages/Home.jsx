0d457458e8b8e31f6aae8c3a2772a696d6451a97
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-09 18:33:38 +0600

    now learnt Grid Layout

--- Learn-MUI/mui/src/components/pages/Home.jsx
+++ Learn-MUI/mui/src/components/pages/Home.jsx
@@ -133,7 +133,7 @@ export default function Home() {
             </Drawer>
             <Main open={open}>
                 <DrawerHeader />
-                <Typography paragraph>
+                <Typography variant="p" className="text-2xl">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
                     facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id donec
                     ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras. Metus
