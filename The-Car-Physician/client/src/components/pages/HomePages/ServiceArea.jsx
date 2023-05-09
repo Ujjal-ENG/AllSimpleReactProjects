56e62b5e8ac2da54129a8bd60272701145f3e223
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-10 03:31:23 +0600

    services done

--- The-Car-Physician/client/src/components/pages/HomePages/ServiceArea.jsx
+++ The-Car-Physician/client/src/components/pages/HomePages/ServiceArea.jsx
@@ -1,9 +1,21 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable operator-linebreak */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable max-len */
 /* eslint-disable no-trailing-spaces */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BsArrowRight } from 'react-icons/bs';
 
 const ServiceArea = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then((res) => res.json())
+            .then((data) => setServices(data));
+    }, []);
+
     return (
         <div>
             <h3 className="text-orange-600 text-xl font-bold text-center">Services</h3>
@@ -11,6 +23,34 @@ const ServiceArea = () => {
             <p className="text-gray-500 tracking-widest text-center mx-auto max-w-3xl">
                 The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
             </p>
+
+            {/* services card */}
+            <div className="grid grid-cols-3 justify-items-center gap-6 py-14">
+                {services &&
+                    services.map((service) => (
+                        <div key={service._id} className="card w-96 border-2">
+                            <figure className="px-10 pt-10">
+                                <img src={service.img} alt={service.title} className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-start text-center space-y-5">
+                                <h2 className="card-title text-2xl">{service.title}</h2>
+
+                                <div className=" flex justify-between items-center w-full">
+                                    <h2 className="card-title text-orange-600">Price: ${service.price}</h2>
+                                    <button type="button" className="btn btn-circle border-none btn-outline">
+                                        <BsArrowRight className="text-3xl text-orange-600 hover:text-white" />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+
+            <div className="flex justify-center items-center w-full">
+                <button type="button" className="btn btn-outline text-orange-500 border-orange-600">
+                    More Services
+                </button>
+            </div>
         </div>
     );
 };
