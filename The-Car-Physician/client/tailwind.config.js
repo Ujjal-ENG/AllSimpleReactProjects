55e0948ebf85f289319d478863bc1069927bd48a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-10 00:49:54 +0600

    added the daisy ui

--- The-Car-Physician/client/tailwind.config.js
+++ The-Car-Physician/client/tailwind.config.js
@@ -1,8 +1,34 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {}
-    },
-    plugins: []
+    daisyui: {
+        themes: [
+          {
+            mytheme: {
+
+   primary: '#FF3811',
+
+   secondary: '#D926AA',
+
+   accent: '#1FB2A5',
+
+   neutral: '#191D24',
+
+   'base-100': '#2A303C',
+
+   info: '#3ABFF8',
+
+   success: '#36D399',
+
+   warning: '#FBBD23',
+
+   error: '#F87272',
+            },
+          },
+        ],
+      },
+    plugins: [require('daisyui')],
 };
