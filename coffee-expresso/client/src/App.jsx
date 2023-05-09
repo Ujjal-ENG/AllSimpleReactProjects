07b54a732d215be506e8a89f936f0fe697e0e445
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-09 23:56:46 +0600

    Feaced some problem

--- coffee-expresso/client/src/App.jsx
+++ coffee-expresso/client/src/App.jsx
@@ -1,29 +1,16 @@
-import { ThemeProvider, useColorMode } from 'daisyui/react';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/layouts/Footer';
 import Navbar from './components/layouts/Navbar';
 
-function MyApp({ Component, pageProps }) {
-    const { colorMode, toggleColorMode } = useColorMode();
-
+const App = () => {
     return (
-        <ThemeProvider color={colorMode === 'dark' ? 'dark' : undefined} onClick={toggleColorMode}>
-            <div className="bg-white dark:bg-gray-800">
-                <Navbar />
-                <Outlet />
-                <Footer />
-            </div>
-        </ThemeProvider>
-    );
-}
-
-function App() {
-    return (
-        <MyApp>
+        <>
+            <Navbar />
             <Outlet />
-        </MyApp>
+            <Footer />
+        </>
     );
-}
+};
 
 export default App;
