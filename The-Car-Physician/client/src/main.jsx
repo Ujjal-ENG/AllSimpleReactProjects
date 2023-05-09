fdd0719932c1d338c4cafefe260a92f02db200b3
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-10 01:12:20 +0600

    set the max width

--- The-Car-Physician/client/src/main.jsx
+++ The-Car-Physician/client/src/main.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import { router } from './routes/App.Routes';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <div className="max-w-7xl mx-auto">
+        <RouterProvider router={router} />
+    </div>
+);
