da0cec07b993c3329b734b7893cbb6ec0d636bc9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-09 23:55:20 +0600

    modifying the app.js

--- coffee-expresso/client/src/App.jsx
+++ coffee-expresso/client/src/App.jsx
@@ -1,16 +1,29 @@
+import { ThemeProvider, useColorMode } from 'daisyui/react';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/layouts/Footer';
 import Navbar from './components/layouts/Navbar';
 
-const App = () => {
+function MyApp({ Component, pageProps }) {
+    const { colorMode, toggleColorMode } = useColorMode();
+
     return (
-        <>
-            <Navbar />
-            <Outlet />
-            <Footer />
-        </>
+        <ThemeProvider color={colorMode === 'dark' ? 'dark' : undefined} onClick={toggleColorMode}>
+            <div className="bg-white dark:bg-gray-800">
+                <Navbar />
+                <Outlet />
+                <Footer />
+            </div>
+        </ThemeProvider>
     );
-};
+}
+
+function App() {
+    return (
+        <MyApp>
+            <Outlet />
+        </MyApp>
+    );
+}
 
 export default App;
