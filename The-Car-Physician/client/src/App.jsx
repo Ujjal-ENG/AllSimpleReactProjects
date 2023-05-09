124d8b82e427fe615eb3717782d3f69e6f75896a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-10 01:08:22 +0600

    initial setup is done

--- The-Car-Physician/client/src/App.jsx
+++ The-Car-Physician/client/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from './components/layouts/shared/Footer';
+import Navbar from './components/layouts/shared/Navbar';
 
 const App = () => {
-    return <div className="text-orange-600 btn-outline btn">App</div>;
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    );
 };
 
 export default App;
--- The-Car-Physician/client/src/routes/App.Routes.jsx
+++ The-Car-Physician/client/src/routes/App.Routes.jsx
@@ -1,1 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-dangle */
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import ErrorPage from '../ErrorPage';
+import Home from '../components/pages/HomePages/Home';
 
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+]);
