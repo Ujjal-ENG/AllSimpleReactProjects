20bd013b7232947c8cc2d7439fd4dc7d4fbda514
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-11 00:11:18 +0600

    succerssfully get all the data

--- The-Car-Physician/backend/index.js
+++ The-Car-Physician/backend/index.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-
+import { MongoClient, ServerApiVersion } from 'mongodb';
 // config env files
 dotenv.config();
 
@@ -16,6 +16,51 @@ app.use(cors());
 app.get('', (req, res) => {
     res.status(200).json({ message: "Hello from The Car Physician Server,What's Up!!!" });
 });
+
+// mongoDB Connection Codes
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.zzrczzq.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const serviceCollection = client.db('theCarPhysician').collection('Services');
+
+        // get all services
+        app.get('/get-services', async (req, res) => {
+            try {
+                const services = await serviceCollection.find().toArray();
+                res.status(200).json({
+                    success: true,
+                    message: 'Successfully Get the all Services!!',
+                    services,
+                });
+            } catch (error) {
+                console.log(error);
+                res.status(404).json({
+                    success: false,
+                    message: 'Error Occured in Get Services Data',
+                    error,
+                });
+            }
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 });
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 // listen port
 const PORT = process.env.PORT || 8080;
