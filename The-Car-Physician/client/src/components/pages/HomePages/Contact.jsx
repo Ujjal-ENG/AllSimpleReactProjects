5287f6abe50151a0b08f33f662ac0da2ea7d5994
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-13 19:17:28 +0600

    added the contact form animation

--- The-Car-Physician/client/src/components/pages/HomePages/Contact.jsx
+++ The-Car-Physician/client/src/components/pages/HomePages/Contact.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-indent-props */
 import React from 'react';
 import { BsCalendar2DateFill, BsFillTelephoneXFill } from 'react-icons/bs';
 import { ImLocation } from 'react-icons/im';
 
 const Contact = () => {
     return (
-        <div className="h-[250px] bg-black rounded-xl w-full my-32 flex justify-evenly items-center">
+        <div
+            data-aos="fade-up"
+            data-aos-offset="100"
+            data-aos-delay="30"
+            data-aos-duration="500"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+            data-aos-anchor-placement="top-center"
+            className="h-[250px] bg-black rounded-xl w-full my-32 flex justify-evenly items-center">
             <div>
                 <div className="flex items-center gap-4">
                     <BsCalendar2DateFill className="text-5xl text-orange-600" />
