ce09487e108b428b19feb1f90f11c194bf12ca56
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-13 19:30:43 +0600

    done the fetures section

--- The-Car-Physician/client/src/components/pages/HomePages/Features.jsx
+++ The-Car-Physician/client/src/components/pages/HomePages/Features.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-indent-props */
 /* eslint-disable max-len */
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
@@ -10,7 +12,16 @@ import Person from '../../../assets/icons/person.svg';
 
 const Features = () => {
     return (
-        <div className="pt-32">
+        <div
+            data-aos="fade-up"
+            data-aos-offset="100"
+            data-aos-delay="30"
+            data-aos-duration="500"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+            data-aos-anchor-placement="top-center"
+            className="pt-32">
             <h3 className="text-orange-600 text-xl font-bold text-center">Core Features</h3>
             <h1 className="text-5xl font-bold tracking-wider text-center py-5">Why Choose Us</h1>
             <p className="text-gray-500 tracking-widest text-center mx-auto max-w-3xl">
