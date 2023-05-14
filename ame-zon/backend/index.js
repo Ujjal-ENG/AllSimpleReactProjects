ea31d29420fd63ec44aff64d37df10db14ac06e6
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-14 22:52:24 +0600

    mongodb server worked perfectly

--- ame-zon/backend/index.js
+++ ame-zon/backend/index.js
@@ -29,14 +29,20 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // server name
+    const productsCollection = client.db("ame-zon").collection("Products")
 
 
-    
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
