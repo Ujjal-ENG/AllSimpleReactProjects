e44ae9816f41cb0a19b9ac9c04bf7772fd891fbe
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-16 01:47:22 +0600

    learn some css and styles

--- email-password-auth/tailwind.config.js
+++ email-password-auth/tailwind.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-unresolved */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  // plugins: [require('daisyui')],
 };
