48aa45089e0d0de8b3c2bedc938cff4d29cd79c2
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-21 21:36:10 +0600

    added another header

--- TailWindComponets/ComPonets/src/components/pages/Home.jsx
+++ TailWindComponets/ComPonets/src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header1 from './Headers/Header1';
 import Header3 from './Headers/Header3';
+import Header4 from './Headers/Header4';
 import Headers2 from './Headers/Headers2';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Header1 />
             <Headers2 />
             <Header3 />
+            <Header4 />
         </div>
     );
 };
