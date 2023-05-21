42f1e2db70e15c47aa43722fdb389dc47046c179
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-22 01:37:56 +0600

    maked the initial server

--- coin-bounce/backend/index.js
+++ coin-bounce/backend/index.js
@@ -1,6 +1,17 @@
 // dependencies
-const http = require('http');
-// app object - module scaffolding
-const app = {};
-// configuration
-app.config = {};
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`App is running on the PORT ${PORT}`);
+});
