730c9776e56049d000458a700ee082da2fd27128
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-23 02:51:00 +0600

    done the call us

---
+++ FlavorFlow/client/src/components/pages/HomePages/CallUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CallUs = () => {
+    return (
+        <section className="py-10 mb-20 rounded-xl bg-gradient-to-r from-fuchsia-600 to-blue-600 sm:py-16">
+            <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                <div className="text-center sm:flex sm:items-center sm:justify-center sm:text-left">
+                    <h2 className="text-4xl font-bold text-white">Call Us: +88 0871 75 00 7777</h2>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default CallUs;
--- FlavorFlow/client/src/components/pages/HomePages/Home.jsx
+++ FlavorFlow/client/src/components/pages/HomePages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import CallUs from './CallUs';
 import Menu from './Menu/Menu';
 import MessageResturant from './MessageResturant';
 import OderOnlineSlider from './OderSlider/OderOnlineSlider';
@@ -11,6 +12,7 @@ const Home = () => {
             <OderOnlineSlider />
             <MessageResturant />
             <Menu />
+            <CallUs />
         </div>
     );
 };
