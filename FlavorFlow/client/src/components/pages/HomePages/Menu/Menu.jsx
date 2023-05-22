5be3e8daf908d19f55eb6612c7317b4482c04049
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-23 02:47:02 +0600

    done the button design and shown the popurlar food

--- FlavorFlow/client/src/components/pages/HomePages/Menu/Menu.jsx
+++ FlavorFlow/client/src/components/pages/HomePages/Menu/Menu.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/button-has-type */
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from 'react';
 import SharedTitle from '../../../layouts/shared/SharedTitle';
@@ -18,6 +21,13 @@ const Menu = () => {
         <div className="my-16">
             <SharedTitle message="Check it Out" title="FROM OUR MENU" />
             <div className="grid grid-cols-1 justify-items-center md:grid-cols-2">{menuData && menuData.map((items) => <MenuItem key={items._id} data={items} />)}</div>
+            <div className="w-full flex justify-center items-center py-10">
+                <button
+                    type="button"
+                    className="border-b-4 rounded-xl border-black px-6 py-2 text-xl font-bold duration-100 transition-all ease-out hover:border-0 active:px-4 active:py-0 active:border-2 active:border-orange-500">
+                    View Full Menu
+                </button>
+            </div>
         </div>
     );
 };
