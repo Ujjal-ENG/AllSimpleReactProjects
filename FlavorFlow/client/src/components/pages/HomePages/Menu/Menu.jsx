62fc366eb4e9348640e69bec5cdf3e3c87ac27bb
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-23 02:32:08 +0600

    popular datapassed

--- FlavorFlow/client/src/components/pages/HomePages/Menu/Menu.jsx
+++ FlavorFlow/client/src/components/pages/HomePages/Menu/Menu.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
 import SharedTitle from '../../../layouts/shared/SharedTitle';
+import MenuItem from './MenuItem';
 
 const Menu = () => {
+    const [menuData, setMenuData] = useState([]);
+    useEffect(() => {
+        fetch('menu.json')
+            .then((res) => res.json())
+            .then((data) => {
+                const popurlarItem = data.filter((el) => el.category === 'popular');
+                return setMenuData(popurlarItem);
+            });
+    }, []);
+    console.log(menuData);
     return (
         <div className="my-16">
             <SharedTitle message="Check it Out" title="FROM OUR MENU" />
+            <div className="grid grid-cols-1 justify-items-center md:grid-cols-2">{menuData && menuData.map((items) => <MenuItem key={items._id} data={items} />)}</div>
         </div>
     );
 };
