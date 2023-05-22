87513145b18954d3787cd1a092f5da7fb5a7d100
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-22 20:40:31 +0600

    login route worked perfectly

--- coin-bounce/backend/controllers/authControllers.js
+++ coin-bounce/backend/controllers/authControllers.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 /* eslint-disable newline-per-chained-call */
 /* eslint-disable prefer-regex-literals */
 import bcrypt from 'bcrypt';
@@ -7,7 +8,49 @@ import { usersModel } from '../models/user.js';
 /* eslint-disable import/prefer-default-export */
 
 // login controller
-export const loginController = async (req, res, next) => {};
+export const loginController = async (req, res, next) => {
+    // 1. validate user input
+    // 2. if validation error. return error
+    // 3. match user name and password
+    // 4. return response
+    try {
+        const userLoginSchema = Joi.object({
+            userName: Joi.string().alphanum().min(3).max(30).required(),
+            password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+        });
+
+        const { error } = userLoginSchema.validate(req.body);
+
+        if (error) {
+            return next(error);
+        }
+
+        const { userName, password } = req.body;
+
+        // match userName
+        const user = await usersModel.findOne({ userName });
+
+        if (!user) {
+            const errord = {
+                status: 401,
+                message: 'Invalid UserName!!',
+            };
+            return next(errord);
+        }
+
+        // match password
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+            const message = { status: 401, message: 'Invalid Password' };
+            return next(message);
+        }
+
+        return res.status(200).json({ success: true, message: 'Login Successfully', user });
+    } catch (error) {
+        return next(error);
+    }
+};
 
 // register controller
 export const registerController = async (req, res, next) => {
@@ -62,5 +105,6 @@ export const registerController = async (req, res, next) => {
         });
     } catch (error) {
         console.log(error);
+        return next(error);
     }
 };
