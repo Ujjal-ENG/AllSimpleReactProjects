1b01d0842471ff179a742331b0de5e3c9c3fa512
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-24 02:15:24 +0600

    done the fetured section

--- FlavorFlow/client/src/components/pages/HomePages/Featured/Featured.jsx
+++ FlavorFlow/client/src/components/pages/HomePages/Featured/Featured.jsx
@@ -13,18 +13,23 @@ const Featured = () => {
                 <SharedTitle message="Check It Out" title="FROM OUR MENU" />
 
                 <div className="flex justify-center items-center gap-20">
-                    <img src={img} alt="featured-img" className="max-w-3xl h-96" />
+                    <div className="relative">
+                        <img src={img} alt="featured-img" className="max-w-3xl h-96" />
+                    </div>
                     <div className="max-w-lg relative">
-                        <div className="absolute bg-slate-600 top-0 right-0" />
-                        <h2 className="text-xl font-semibold leading-tight text-white py-3">October 21, 2024</h2>
-                        <h2 className="text-xl font-semibold leading-tight text-white uppercase">Where can i get some?</h2>
-                        <p className="mt-4 text-base text-gray-200">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem possimus voluptatem nam accusamus nulla placeat veritatis nihil repellendus sit explicabo. Rem reiciendis
-                            quibusdam eum dignissimos repellat mollitia neque. Itaque doloribus quibusdam accusamus numquam debitis assumenda culpa, provident illo velit odio omnis? Quod doloribus quo
-                            exercitationem eligendi distinctio expedita similique aliquid.
-                        </p>
+                        <div className="overlay" />
+                        <div className="relative">
+                            <h2 className="text-xl font-semibold leading-tight text-white py-3">October 21, 2024</h2>
+                            <h2 className="text-xl font-semibold leading-tight text-white uppercase">Where can I get some?</h2>
+                            <p className="mt-4 text-base text-gray-200">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem possimus voluptatem nam accusamus nulla placeat veritatis nihil repellendus sit explicabo. Rem
+                                reiciendis quibusdam eum dignissimos repellat mollitia neque. Itaque doloribus quibusdam accusamus numquam debitis assumenda culpa, provident illo velit odio omnis?
+                                Quod doloribus quo exercitationem eligendi distinctio expedita similique aliquid.
+                            </p>
+                        </div>
                     </div>
                 </div>
+
                 <button type="button" className="btn btn-wide mt-8 text-white btn-outline border-0 border-b-4">
                     Read More
                 </button>
