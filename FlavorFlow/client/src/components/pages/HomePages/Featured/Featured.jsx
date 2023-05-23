6138c281dff2fc0677a8d87441df67d919c1de08
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-24 01:13:29 +0600

    Start to again this code

--- FlavorFlow/client/src/components/pages/HomePages/Featured/Featured.jsx
+++ FlavorFlow/client/src/components/pages/HomePages/Featured/Featured.jsx
@@ -4,41 +4,31 @@
 import React from 'react';
 import img from '../../../../assets/home/featured.jpg';
 import SharedTitle from '../../../layouts/shared/SharedTitle';
+import './featured.css';
 
 const Featured = () => {
     return (
-        <div className="mt-10 mb-20 ">
-            <section className="relative py-10 overflow-hidden bg-black sm:py-16 lg:py-24 xl:py-32">
-                <div className="absolute inset-0">
-                    <img className="object-cover w-full h-full md:object-left md:scale-150 md:origin-top-left" src={img} alt="featured-img" />
-                </div>
+        <div className="mt-10 mb-20 bg-fixed featured-item pb-20">
+            <div className=" px-4 w-full flex justify-center items-center flex-col">
+                <SharedTitle message="Check It Out" title="FROM OUR MENU" />
 
-                <div className="absolute inset-0 hidden bg-gradient-to-r md:block from-black to-transparent" />
-
-                <div className="absolute inset-0 block bg-black/60 md:hidden" />
-
-                <div className="relative px-4 mx-auto w-full flex justify-center items-center flex-col">
-                    <SharedTitle message="Check It Out" title="FROM OUR MENU" />
-
-                    <div className="flex justify-center items-center gap-20">
-                        <img src={img} alt="featured-img" className="max-w-3xl h-96" />
-                        <div className="max-w-lg">
-                            <h2 className="text-xl font-semibold leading-tight text-white py-3">October 21, 2024</h2>
-                            <h2 className="text-xl font-semibold leading-tight text-white uppercase">Where can i get some?</h2>
-                            <p className="mt-4 text-base text-gray-200">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem possimus voluptatem nam accusamus nulla placeat veritatis nihil repellendus sit explicabo. Rem
-                                reiciendis quibusdam eum dignissimos repellat mollitia neque. Itaque doloribus quibusdam accusamus numquam debitis assumenda culpa, provident illo velit odio omnis?
-                                Quod doloribus quo exercitationem eligendi distinctio expedita similique aliquid.
-                            </p>
-                        </div>
+                <div className="flex justify-center items-center gap-20">
+                    <img src={img} alt="featured-img" className="max-w-3xl h-96" />
+                    <div className="max-w-lg relative">
+                        <div className="absolute bg-slate-600 top-0 right-0" />
+                        <h2 className="text-xl font-semibold leading-tight text-white py-3">October 21, 2024</h2>
+                        <h2 className="text-xl font-semibold leading-tight text-white uppercase">Where can i get some?</h2>
+                        <p className="mt-4 text-base text-gray-200">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem possimus voluptatem nam accusamus nulla placeat veritatis nihil repellendus sit explicabo. Rem reiciendis
+                            quibusdam eum dignissimos repellat mollitia neque. Itaque doloribus quibusdam accusamus numquam debitis assumenda culpa, provident illo velit odio omnis? Quod doloribus quo
+                            exercitationem eligendi distinctio expedita similique aliquid.
+                        </p>
                     </div>
-                    <button
-                        type="button"
-                        className="border-b-4 rounded-xl border-white mt-11 px-6 py-2 text-xl font-bold duration-100 transition-all ease-out hover:border-0 active:px-4 active:py-0 active:border-2 text-white active:border-orange-500">
-                        Read More
-                    </button>
                 </div>
-            </section>
+                <button type="button" className="btn btn-wide mt-8 text-white btn-outline border-0 border-b-4">
+                    Read More
+                </button>
+            </div>
         </div>
     );
 };
