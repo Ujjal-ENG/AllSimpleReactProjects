86e82b311a63f908ad25ec296c1f85c519aca343
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-24 14:25:32 +0600

    done the parallex effect

--- FlavorFlow/client/src/components/layouts/cover/Cover.jsx
+++ FlavorFlow/client/src/components/layouts/cover/Cover.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Parallax } from 'react-parallax';
 
 const Cover = ({ img, title, message }) => {
     return (
-        <div className="hero h-[700px]" style={{ backgroundImage: `url("${img}")` }}>
-            <div className="hero max-w-md  mx-auto text-center text-neutral-content">
-                <div className="hero-overlay bg-opacity-50" />
-                <div className="max-w-md py-10 ">
-                    <h1 className="mb-5 text-5xl font-bold">{title}</h1>
-                    <p className="mb-5">{message}</p>
+        <Parallax blur={{ min: -10, max: 15 }} bgImage={img} bgImageAlt="the dog" strength={-200}>
+            <div className="hero h-[700px]">
+                <div className="hero max-w-md  mx-auto text-center text-neutral-content">
+                    <div className="hero-overlay bg-opacity-50" />
+                    <div className="max-w-md py-10 ">
+                        <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+                        <p className="mb-5">{message}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div style={{ height: '10px' }} />
+        </Parallax>
     );
 };
 
