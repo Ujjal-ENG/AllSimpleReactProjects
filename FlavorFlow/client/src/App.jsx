212a88077f948172f4416fce117bd039692f469e
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-26 01:35:36 +0600

    Added the aos initialization

--- FlavorFlow/client/src/App.jsx
+++ FlavorFlow/client/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 import React, { useEffect } from 'react';
@@ -11,9 +12,30 @@ const App = () => {
     const location = useLocation();
     const isLogin = location.pathname.includes('/login');
     const isRegister = location.pathname.includes('/register');
+
     useEffect(() => {
-        // ..
-        AOS.init();
+        AOS.init({
+            offset: 300,
+            duration: 800,
+            easing: 'ease-in-out',
+            delay: 30,
+            once: false,
+            disable() {
+                const maxWidth = 1024;
+                return window.innerWidth < maxWidth;
+            }
+        });
+
+        // Refresh AOS when an element enters the viewport
+        const handleAOSIn = () => {
+            AOS.refresh();
+        };
+
+        window.addEventListener('aos:in', handleAOSIn);
+
+        return () => {
+            window.removeEventListener('aos:in', handleAOSIn);
+        };
     }, []);
     return (
         <AuthProvider>
