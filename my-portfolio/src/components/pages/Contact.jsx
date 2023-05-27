6ab2978e3a9aa1b8fec1f134de2e1c2e7d74ccfe
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-27 18:48:03 +0600

    chnage name link

--- my-portfolio/src/components/pages/Contact.jsx
+++ my-portfolio/src/components/pages/Contact.jsx
@@ -147,7 +147,7 @@ const Contact = () => {
                                                         />
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                                     </svg>
-                                                    <span className="block ml-3 text-base text-gray-900"> 8502 Preston Rd. Inglewood, Maine 98380, USA </span>
+                                                    <span className="block ml-3 text-base text-gray-900"> Mirpur 10,Dhaka </span>
                                                 </div>
 
                                                 <div className="flex items-start">
@@ -159,7 +159,7 @@ const Contact = () => {
                                                             d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                                                         />
                                                     </svg>
-                                                    <span className="block ml-3 text-base text-gray-900"> kenzi.lawson@example.com </span>
+                                                    <span className="block ml-3 text-base text-gray-900"> ujjalroy7862@gmail.com </span>
                                                 </div>
 
                                                 <div className="flex items-start">
@@ -172,8 +172,7 @@ const Contact = () => {
                                                         />
                                                     </svg>
                                                     <div className="ml-3">
-                                                        <span className="block text-base text-gray-900"> (316) 555-0116 </span>
-                                                        <span className="block mt-1 text-base text-gray-900"> (316) 555-0116 </span>
+                                                        <span className="block text-base text-gray-900"> +880179842140 </span>
                                                     </div>
                                                 </div>
                                             </div>
@@ -187,8 +186,8 @@ const Contact = () => {
                                                 <ul className="flex items-center justify-end space-x-3">
                                                     <li>
                                                         <a
-                                                            href="#"
-                                                            title=""
+                                                            href="https://www.facebook.com/ujjal.roy.7862"
+                                                            target="_blank"
                                                             className="
                                                     flex
                                                     items-center
@@ -206,35 +205,8 @@ const Contact = () => {
                                                     focus:text-white
                                                     hover:bg-blue-600 hover:border-blue-600
                                                     focus:border-blue-600
-                                                ">
-                                                            <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                                                <path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z" />
-                                                            </svg>
-                                                        </a>
-                                                    </li>
-
-                                                    <li>
-                                                        <a
-                                                            href="#"
-                                                            title=""
-                                                            className="
-                                                    flex
-                                                    items-center
-                                                    justify-center
-                                                    text-gray-800
-                                                    transition-all
-                                                    duration-200
-                                                    bg-transparent
-                                                    border border-gray-300
-                                                    rounded-full
-                                                    w-7
-                                                    h-7
-                                                    focus:bg-blue-600
-                                                    hover:text-white
-                                                    focus:text-white
-                                                    hover:bg-blue-600 hover:border-blue-600
-                                                    focus:border-blue-600
-                                                ">
+                                                "
+                                                            rel="noreferrer">
                                                             <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                                                 <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z" />
                                                             </svg>
@@ -243,38 +215,8 @@ const Contact = () => {
 
                                                     <li>
                                                         <a
-                                                            href="#"
-                                                            title=""
-                                                            className="
-                                                    flex
-                                                    items-center
-                                                    justify-center
-                                                    text-gray-800
-                                                    transition-all
-                                                    duration-200
-                                                    bg-transparent
-                                                    border border-gray-300
-                                                    rounded-full
-                                                    w-7
-                                                    h-7
-                                                    focus:bg-blue-600
-                                                    hover:text-white
-                                                    focus:text-white
-                                                    hover:bg-blue-600 hover:border-blue-600
-                                                    focus:border-blue-600
-                                                ">
-                                                            <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                                                <path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z" />
-                                                                <circle cx="16.806" cy="7.207" r="1.078" />
-                                                                <path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z" />
-                                                            </svg>
-                                                        </a>
-                                                    </li>
-
-                                                    <li>
-                                                        <a
-                                                            href="#"
-                                                            title=""
+                                                            href="https://github.com/Ujjal-ENG"
+                                                            target="_blank"
                                                             className="
                                                     flex
                                                     items-center
@@ -293,7 +235,8 @@ const Contact = () => {
                                                     hover:bg-blue-600 hover:border-blue-600
                                                     focus:border-blue-600
                                                 // eslint-disable-next-line react/jsx-closing-bracket-location
-                                                ">
+                                                "
+                                                            rel="noreferrer">
                                                             <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                                                 <path
                                                                     fillRule="evenodd"
