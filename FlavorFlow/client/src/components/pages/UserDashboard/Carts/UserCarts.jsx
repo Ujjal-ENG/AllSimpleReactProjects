8bd6d567b070bcd2857bb7a2b9f2f02ed4e56842
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-28 04:17:13 +0600

    Select the title based on the routes

--- FlavorFlow/client/src/components/pages/UserDashboard/Carts/UserCarts.jsx
+++ FlavorFlow/client/src/components/pages/UserDashboard/Carts/UserCarts.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const UserCarts = () => {
-    return <div>UserCarts</div>;
+    return (
+        <div>
+            <Helmet>
+                <title>Flavor|Flow - My Cart</title>
+            </Helmet>
+            UserCarts
+        </div>
+    );
 };
 
 export default UserCarts;
--- FlavorFlow/client/src/components/pages/UserDashboard/Home/DashboardHome.jsx
+++ FlavorFlow/client/src/components/pages/UserDashboard/Home/DashboardHome.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const DashboardHome = () => {
-    return <div>DashboardHome</div>;
+    return (
+        <div>
+            <Helmet>
+                <title>Flavor|Flow - User Dashboard</title>
+            </Helmet>
+            DashboardHome
+        </div>
+    );
 };
 
 export default DashboardHome;
