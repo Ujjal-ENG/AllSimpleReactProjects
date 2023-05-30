1d7df5e9dcade0196e6b1c09ed0eb5aa325f50a7
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-30 15:50:18 +0600

    logo section is done

--- airbnb-clone/client/src/components/layouts/routesLayout/MainLayout.jsx
+++ airbnb-clone/client/src/components/layouts/routesLayout/MainLayout.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../sharedLayout/Footer';
+import Navbar from '../sharedLayout/Navbar';
 
 const MainLayout = () => {
-    return <div>MainLayout</div>;
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
 };
 
 export default MainLayout;
--- airbnb-clone/client/src/components/layouts/sharedLayout/Navbar.jsx
+++ airbnb-clone/client/src/components/layouts/sharedLayout/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/images/logo.png';
+import Container from './Container';
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div className="fixed w-full bg-white z-30 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    <div className="flex justify-between items-center gap-3 md:gap-0">
+                        <Link to="/" className="hidden md:block">
+                            <img src={logo} alt="logo" className="w-32 h-10 cursor-pointer" />
+                        </Link>
+                        <div>Search</div>
+                        <div>Menu</div>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
