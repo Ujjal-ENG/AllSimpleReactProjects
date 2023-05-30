64dcc59d82d77fd11f3e6db384da1f39a7178144
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-30 16:27:36 +0600

    Done the avatar image

--- airbnb-clone/client/src/components/layouts/sharedLayout/Navbar/MenuDropdown.jsx
+++ airbnb-clone/client/src/components/layouts/sharedLayout/Navbar/MenuDropdown.jsx
@@ -2,6 +2,7 @@
 import { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import avatar from '../../../../assets/images/placeholder.jpg';
 import useAuth from '../../../../hooks/useAuth';
 
 const MenuDropdown = () => {
@@ -17,14 +18,14 @@ const MenuDropdown = () => {
                 <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                     <AiOutlineMenu />
                     <div className="hidden md:block">
-                        <img src="" alt="" className="w-14 h-14 rounded-full" />
+                        <img src={avatar} alt="avatar" className="w-8 h-8 rounded-full" />
                     </div>
                 </div>
             </div>
             {isOpen && (
                 <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
                     <div className="flex flex-col cursor-pointer">
-                        <Link to="/" className="block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+                        <Link to="/" className="block  px-4 py-3 hover:bg-neutral-100 transition font-semibold">
                             Home
                         </Link>
                         {user ? (
