955f5ce8bac2d699c76844c947ae463dab07d4f3
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-30 17:46:27 +0600

    done login and signup page

--- airbnb-clone/client/src/routes/App.Routes.jsx
+++ airbnb-clone/client/src/routes/App.Routes.jsx
@@ -2,7 +2,9 @@
 /* eslint-disable comma-dangle */
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../components/layouts/routesLayout/MainLayout';
-import Home from '../components/pages/Home';
+import Home from '../components/pages/Home/Home';
+import Login from '../components/pages/Login&registration/Login';
+import Register from '../components/pages/Login&registration/Register';
 
 export const router = createBrowserRouter([
     {
@@ -14,5 +16,13 @@ export const router = createBrowserRouter([
                 element: <Home />
             }
         ]
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/signup',
+        element: <Register />
     }
 ]);
