559b0b5c5bb91517a6447b9664d38d71a96ddfb3
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-30 22:13:39 +0600

    try to fixed a problem

--- assignment-task/src/App.jsx
+++ assignment-task/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Home from './components/pages/Home';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
     return (
         <div>
-            <Home />
+            <Outlet />
         </div>
     );
 };
--- assignment-task/src/routes/App.Routes.jsx
+++ assignment-task/src/routes/App.Routes.jsx
@@ -3,6 +3,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import Home from '../components/pages/Home';
+import Test from '../components/pages/Test';
 
 export const router = createBrowserRouter([
     {
@@ -11,7 +12,13 @@ export const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home />
+                element: <Home />,
+                children: [
+                    {
+                        path: '/test',
+                        element: <Test />
+                    }
+                ]
             }
         ]
     }
