c060508f90bcd1d9061e7fa1cf71c263b1a4bb37
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-31 01:07:25 +0600

    Removed some unsual options

--- assignment-task/src/components/pages/Test.jsx
+++ assignment-task/src/components/pages/Test.jsx
@@ -7,12 +7,13 @@ import ReactTablesCharts from './chart/ReactTablesCharts';
 
 const Test = () => {
     return (
-        <div className="px-4 py-2 bg-slate-100">
+        <div className=" bg-slate-100">
             <Navbar />
-            <div className="flex justify-between">
+            <div className="flex justify-evenly">
                 <Rates />
+                <h1>hell</h1>
             </div>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-3 gap-4 px-4 py-2">
                 <ReachChart />
                 <ReactTablesCharts />
                 <FilteredTable />
