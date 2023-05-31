64fc57c7742b86f81a40e9e0bcae0c59c89a8f47
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-31 10:21:15 +0600

    Try to some portion

--- assignment-task/src/components/pages/SideBar.jsx
+++ assignment-task/src/components/pages/SideBar.jsx
@@ -17,7 +17,9 @@ const SideBar = () => {
                     <label htmlFor="my-drawer-2" className="drawer-overlay" />
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
                         <li>
-                            <NavLink to="/text">Test</NavLink>
+                            <NavLink to="/text" className="btn btn-active">
+                                Test
+                            </NavLink>
                         </li>
                         <li>
                             <a>Sidebar Item 2</a>
