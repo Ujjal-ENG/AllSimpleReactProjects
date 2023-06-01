19a05c91656923eb0852bbf6a3f6483cad136cc3
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-01 11:00:55 +0600

    added title

--- FlavorFlow/client/src/components/pages/AdminDashboard/AddItem.jsx
+++ FlavorFlow/client/src/components/pages/AdminDashboard/AddItem.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import SharedTitle from '../../layouts/shared/SharedTitle';
 
 const AddItem = () => {
-    return <div>AddItem</div>;
+    return (
+        <div className="md:-ml-36 -ml-0 ">
+            <Helmet>
+                <title>Flavor|Flow - Add Item</title>
+            </Helmet>
+            <SharedTitle title="ADD AN ITEM" message={"What's New?"} />
+        </div>
+    );
 };
 
 export default AddItem;
