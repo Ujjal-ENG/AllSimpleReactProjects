7017b049e6d6fac43957fc093d33696436091b37
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-02 17:57:45 +0600

    added title

--- FlavorFlow/client/src/components/pages/UserDashboard/Payment/Payment.jsx
+++ FlavorFlow/client/src/components/pages/UserDashboard/Payment/Payment.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import SharedTitle from '../../../layouts/shared/SharedTitle';
 
 const Payment = () => {
-    return <div>Payment</div>;
+    return (
+        <div className="md:-ml-60 -ml-0">
+            <Helmet>
+                <title>Flavor|Flow - Payment</title>
+            </Helmet>
+            <SharedTitle title="PAYMENT" message="Please proceed to Process!" />
+        </div>
+    );
 };
 
 export default Payment;
