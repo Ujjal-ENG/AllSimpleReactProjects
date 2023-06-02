1c8e5c2f5ae058801a75520283ee3861ace1865c
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-02 17:21:22 +0600

    link up the full mennu section

--- FlavorFlow/client/src/components/pages/HomePages/Menu/Menu.jsx
+++ FlavorFlow/client/src/components/pages/HomePages/Menu/Menu.jsx
@@ -3,6 +3,7 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import SharedTitle from '../../../layouts/shared/SharedTitle';
 import MenuItem from './MenuItem';
 
@@ -19,11 +20,11 @@ const Menu = () => {
         <div className="my-16">
             <SharedTitle message="Check it Out" title="FROM OUR MENU" />
             <div className="grid grid-cols-1 justify-items-center md:grid-cols-2">{popular && popular.map((items) => <MenuItem key={items._id} data={items} />)}</div>
-            <div className="w-full flex justify-center items-center py-10">
+            <Link to="/our-menu" className="w-full flex justify-center items-center py-10">
                 <button type="button" className="border-b-4 btn btn-outline btn-wide border-b-orange-500 border-0">
                     View Full Menu
                 </button>
-            </div>
+            </Link>
         </div>
     );
 };
