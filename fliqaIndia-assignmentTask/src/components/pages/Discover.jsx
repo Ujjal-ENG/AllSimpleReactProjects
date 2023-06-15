17e430617ea8f714e4bc00073b9148cee10e6aeb
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-15 21:42:35 +0600

    done the design

--- fliqaIndia-assignmentTask/src/components/pages/Discover.jsx
+++ fliqaIndia-assignmentTask/src/components/pages/Discover.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import bannerImg from '../../assets/banner2.png';
+import img1 from '../../assets/discover1.png';
+import img2 from '../../assets/discover2.png';
+import img3 from '../../assets/discover3.png';
+import img4 from '../../assets/discover4.png';
 
 const Discover = () => {
     return (
@@ -13,7 +17,7 @@ const Discover = () => {
 
                 <div className="absolute inset-0 block bg-black/60 md:hidden" />
 
-                <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                <div className="relative px-8  max-w-7xl">
                     <div className="text-center lg:w-2/3 xl:w-1/3 md:text-left pb-32">
                         <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl uppercase">Discover</h2>
                         <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl uppercase">Graphic Design Ideas</h2>
@@ -25,6 +29,19 @@ const Discover = () => {
                         <button type="button" className="btn -ml-14 bg-black text-white z-10 px-24 rounded-full">
                             Search
                         </button>
+                    </div>
+
+                    <div className="absolute top-0 left-2/3">
+                        <img src={img1} alt="img1" className="border-8 border-black" />
+                    </div>
+                    <div className="absolute -top-20 -right-10">
+                        <img src={img2} alt="img2" className="border-8 border-black" />
+                    </div>
+                    <div className="absolute top-40 -right-72">
+                        <img src={img3} alt="img3" className="border-8 border-black" />
+                    </div>
+                    <div className="absolute -top-24 -right-1/3">
+                        <img src={img4} alt="img4" className="border-8 border-black" />
                     </div>
                 </div>
             </section>
