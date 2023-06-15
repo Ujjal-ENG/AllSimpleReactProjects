eab71b8659e65301e0b6e94c6c98cd46f3cafa50
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-15 22:24:20 +0600

    done the blogs part

--- fliqaIndia-assignmentTask/src/components/pages/Blogs.jsx
+++ fliqaIndia-assignmentTask/src/components/pages/Blogs.jsx
@@ -1,56 +1,65 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import goa1 from '../../assets/goa.png';
 import goa2 from '../../assets/goa2.png';
 
 const Blogs = () => {
     return (
-        <div className="bg-gradient-to-r  from-teal-400 to-teal-100 relative">
-            <div className="flex justify-between items-center">
-                <div>
-                    <h2 className="text-5xl font-semibold tracking-wider px-10   text-teal-700 pt-10">Our Blogs</h2>
-                    <p className="pb-[400px] px-10 text-xl tracking-wider text-black">Check out our Latest Blog</p>
+        <>
+            <div className="py-10 flex justify-center items-center ">
+                <h1 className="text-3xl tracking-widest">
+                    Popular: <span className="text-gray-500 ml-10">Website Design Logo Design Brochure Label Design</span>
+                </h1>
+            </div>
+            <div className="border-b-2 border-black max-w-7xl mx-auto" />
+            <div className="bg-gradient-to-r  from-teal-400 to-teal-100 mt-10 relative">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h2 className="text-5xl font-semibold tracking-wider px-10   text-teal-700 pt-10">Our Blogs</h2>
+                        <p className="pb-[400px] px-10 text-xl tracking-wider text-black">Check out our Latest Blog</p>
+                    </div>
                 </div>
-            </div>
-            <div className="absolute bottom-4 pl-10">
-                <img src={goa1} alt="goa1" />
-            </div>
-            <div className="absolute bottom-4 pl-[650px]">
-                <img src={goa2} alt="goa2" />
-            </div>
-
-            <div className="absolute w-8 h-8 rounded-full bg-white top-24 left-3/4" />
-            <div className="absolute w-4 h-4 rounded-full bg-white top-14 left-3/4" />
-
-            <div className="right-0 top-36 absolute">
-                <div className="flex justify-between items-center gap-36">
-                    <h1 className="text-2xl font-bold">Trending Now</h1>
-                    <h1 className="text-2xl font-bold">View All</h1>
+                <div className="absolute bottom-4 pl-10">
+                    <img src={goa1} alt="goa1" />
+                </div>
+                <div className="absolute bottom-4 pl-[650px]">
+                    <img src={goa2} alt="goa2" />
                 </div>
 
-                <div className="py-5 border-b-2 border-black flex justify-between">
-                    <div className="avatar">
-                        <div className="w-32 rounded">
-                            <img src={goa1} alt="goa1" />
+                <div className="absolute w-8 h-8 rounded-full bg-white top-24 left-3/4" />
+                <div className="absolute w-4 h-4 rounded-full bg-white top-14 left-3/4" />
+
+                <div className="right-0 top-36 absolute">
+                    <div className="flex justify-between items-center gap-36">
+                        <h1 className="text-2xl font-bold">Trending Now</h1>
+                        <h1 className="text-2xl font-bold">View All</h1>
+                    </div>
+
+                    <div className="py-5 border-b-2 border-black flex justify-between">
+                        <div className="avatar">
+                            <div className="w-32 rounded">
+                                <img src={goa1} alt="goa1" />
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className="text-xl font-bold w-64">Creative Industries are on the verge of depletion due to COVID-19</h1>
+                            <p className="text-xs  w-64">In a period where many industries remain highly unclear, others want to grasp how COVID-19 impact</p>
                         </div>
                     </div>
-                    <div>
-                        <h1 className="text-xl font-bold w-64">Creative Industries are on the verge of depletion due to COVID-19</h1>
-                        <p className="text-xs  w-64">In a period where many industries remain highly unclear, others want to grasp how COVID-19 impact</p>
-                    </div>
-                </div>
-                <div className="py-5 border-b-2 border-black flex justify-between">
-                    <div className="avatar">
-                        <div className="w-32 rounded">
-                            <img src={goa2} alt="goa2" />
+                    <div className="py-5 border-b-2 border-black flex justify-between">
+                        <div className="avatar">
+                            <div className="w-32 rounded">
+                                <img src={goa2} alt="goa2" />
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <h1 className="text-xl font-bold w-64">Creative Industries are on the verge of depletion due to COVID-19</h1>
-                        <p className="text-xs  w-64">In a period where many industries remain highly unclear, others want to grasp how COVID-19 impact</p>
+                        <div>
+                            <h1 className="text-xl font-bold w-64">Creative Industries are on the verge of depletion due to COVID-19</h1>
+                            <p className="text-xs  w-64">In a period where many industries remain highly unclear, others want to grasp how COVID-19 impact</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
