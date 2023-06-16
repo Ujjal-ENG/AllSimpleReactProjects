1c030b6ed046a667fcfb25d0ed337ae499c51184
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-16 17:05:16 +0600

    done the another section

--- fliqaIndia-assignmentTask/src/components/pages/Reviews.jsx
+++ fliqaIndia-assignmentTask/src/components/pages/Reviews.jsx
@@ -8,12 +8,12 @@ const Reviews = () => {
         <div className="border-b-2 border-black pb-8">
             <h1 className="text-[#4C696D] text-4xl font-bold text-center pt-5 pb-2">Testimonials</h1>
             <h1 className="text-[#4C696D] text-xl font-semibold text-center  pb-2">Words from our Clients</h1>
-            <div className="grid grid-cols-3 justify-items-center">
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center">
                 <div className="col-span-1">
-                    <img src={img} alt="group" className="pl-20" />
+                    <img src={img} alt="group" className="md:pl-20 pl-0" />
                 </div>
-                <div className="col-span-2 w-full">
-                    <div className="flex justify-end gap-2">
+                <div className="col-span-2 w-full pt-10 md:pt-0">
+                    <div className="flex justify-center md:flex-row flex-col md:justify-end gap-10 md:gap-2 px-10 md:px-0">
                         <Button>Google</Button>
                         <button type="button" className="btn border-2 text-black bg-white border-black  rounded-[24px] text-xl px-8">
                             Facebook
@@ -26,7 +26,7 @@ const Reviews = () => {
                         </button>
                     </div>
 
-                    <div className="grid grid-cols-4 justify-items-center pt-10 bg-gradient-to-t from-transparent via-[#BADCDC] to-transparent">
+                    <div className="grid grid-cols-1 md:grid-cols-4 justify-items-center pt-10 bg-gradient-to-t from-transparent via-[#BADCDC] to-transparent">
                         <div className="w-[220px] h-[300px] relative">
                             <div className="w-[198px] h-[275px] bg-[#416F75] text-white pt-20 px-3 rounded-md">
                                 <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate perspiciatis distinctio,</p>
