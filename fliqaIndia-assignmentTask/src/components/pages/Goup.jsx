98790780e32efe2cb6888c36219432a91c29ef2e
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-16 17:02:32 +0600

    annother seciton is done

--- fliqaIndia-assignmentTask/src/components/pages/Goup.jsx
+++ fliqaIndia-assignmentTask/src/components/pages/Goup.jsx
@@ -4,7 +4,7 @@ import group1 from '../../assets/group1.png';
 
 const Goup = () => {
     return (
-        <div className="bg-gradient-to-r  from-teal-400 to-teal-100 mt-10 flex justify-evenly items-center py-10">
+        <div className="bg-gradient-to-r  from-teal-400 to-teal-100 mt-10 flex md:flex-row flex-col gap-8 md:gap-0 px-10 md:px-0 justify-evenly items-center py-10">
             <img src={group1} alt="group1" />
             <img src={group2} alt="group2" />
         </div>
