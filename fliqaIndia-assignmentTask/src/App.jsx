ad614140279b02c39b5340be86e49a2891351168
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-16 15:10:48 +0600

    added animation

--- fliqaIndia-assignmentTask/src/App.jsx
+++ fliqaIndia-assignmentTask/src/App.jsx
@@ -1,8 +1,16 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/layouts/shared/Navbar';
 
 const App = () => {
+    AOS.init({
+        offset: 200,
+        duration: 600,
+        easing: 'ease-in-sine',
+        delay: 100
+    });
     return (
         <div>
             <Navbar />
--- fliqaIndia-assignmentTask/src/components/pages/Home.jsx
+++ fliqaIndia-assignmentTask/src/components/pages/Home.jsx
@@ -15,16 +15,36 @@ const Home = () => {
     return (
         <div>
             <BannerImg />
-            <Header />
-            <TrustedAssociate />
-            <Featured />
-            <Discover />
-            <Blogs />
-            <Offers />
-            <Reviews />
-            <Goup />
-            <OurSuccess />
-            <Bannner2 />
+            <div data-aos="zoom-in-up">
+                <Header />
+            </div>
+            <div data-aos="zoom-in-up">
+                <TrustedAssociate />
+            </div>
+            <div data-aos="fade-up">
+                <Featured />
+            </div>
+            <div data-aos="fade-up">
+                <Discover />
+            </div>
+            <div data-aos="zoom-in-up">
+                <Blogs />
+            </div>
+            <div data-aos="fade-up">
+                <Offers />
+            </div>
+            <div data-aos="fade-down">
+                <Reviews />
+            </div>
+            <div data-aos="zoom-in-up">
+                <Goup />
+            </div>
+            <div data-aos="fade-up">
+                <OurSuccess />
+            </div>
+            <div data-aos="fade-down">
+                <Bannner2 />
+            </div>
         </div>
     );
 };
