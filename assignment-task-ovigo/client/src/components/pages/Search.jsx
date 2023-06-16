57f539317378691e903a5257d444f1b37c4dfde2
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-16 20:43:42 +0600

    done the search box

---
+++ assignment-task-ovigo/client/src/components/pages/Search.jsx
@@ -0,0 +1,82 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-props-no-spreading */
+
+import { enGB } from 'date-fns/locale';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaBed } from 'react-icons/fa';
+import { IoIosMan } from 'react-icons/io';
+import { MdDateRange } from 'react-icons/md';
+import { DateRangePicker, END_DATE, START_DATE } from 'react-nice-dates';
+import 'react-nice-dates/build/style.css';
+
+const Search = () => {
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        data.startDate = startDate;
+        data.endDate = endDate;
+        console.log(data);
+    };
+
+    return (
+        <div className="max-w-7xl mx-auto bg-white border-black duration-150 transition-all ease-in-out hover:border-orange-400 hover:border-4 border-2 rounded-md  -mt-10">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex justify-evenly items-center">
+                <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <FaBed size={24} />
+                    </div>
+                    <input
+                        {...register('place', { required: true })}
+                        placeholder="Where are you going?"
+                        className="block font-bold w-full py-4 pl-10 pr-9 text-black placeholder-gray-500 transition-all duration-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                    />
+                </div>
+
+                <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
+                    <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+                        <MdDateRange size={24} />
+                    </div>
+                    <DateRangePicker
+                        startDate={startDate}
+                        endDate={endDate}
+                        onStartDateChange={setStartDate}
+                        onEndDateChange={setEndDate}
+                        minimumDate={new Date()}
+                        minimumLength={1}
+                        format="dd MMM yyyy"
+                        locale={enGB}>
+                        {({ startDateInputProps, endDateInputProps, focus }) => (
+                            <div className="date-range">
+                                <input className={`input${focus === START_DATE ? ' -focused' : ''} pl-10`} {...startDateInputProps} required />
+                                <span className="date-range_arrow" />
+                                <input className={`input${focus === END_DATE ? ' -focused' : ''}`} {...endDateInputProps} placeholder="08/06/2022" required />
+                            </div>
+                        )}
+                    </DateRangePicker>
+                </div>
+
+                <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <IoIosMan size={24} />
+                    </div>
+                    <input
+                        {...register('room-details', { required: true })}
+                        placeholder="1 adult . 0 children . 1 room"
+                        className="block font-bold w-full py-4 pl-10 pr-9 text-black placeholder-gray-500 transition-all duration-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                    />
+                </div>
+
+                <button type="submit" className="btn border-none text-xl font-bold text-white bg-blue-700 px-5">
+                    Search
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Search;
