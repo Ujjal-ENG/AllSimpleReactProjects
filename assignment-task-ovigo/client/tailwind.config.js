4e65b2142684f7acc336db176c111920f60cb9d2
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-16 19:24:51 +0600

    done the header page

--- assignment-task-ovigo/client/tailwind.config.js
+++ assignment-task-ovigo/client/tailwind.config.js
@@ -1,8 +1,11 @@
+/* eslint-disable quotes */
+/* eslint-disable import/no-extraneous-dependencies */
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {},
     },
-    plugins: []
+    // eslint-disable-next-line global-require
+    plugins: [require("daisyui")],
 };
