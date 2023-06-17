8395c0700cbac535dadaed80f33bbf763f2e4cf6
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-17 15:35:17 +0600

    user great is done

--- assignment-task-ovigo/client/src/components/pages/UserDashboard/Home/DashboardHome.jsx
+++ assignment-task-ovigo/client/src/components/pages/UserDashboard/Home/DashboardHome.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import useAuth from '../../../../hooks/useAuth';
 
 const DashboardHome = () => {
+    const { userInfo } = useAuth();
     return (
-        <div>
+        <div className="max-w-5xl -ml-36">
             <Helmet>
                 <title>AssignmentTask - User Dashboard</title>
             </Helmet>
-            DashboardHome
+            <section className="py-10 bg-white ">
+                <div className="max-w-4xl px-4 mx-auto sm:px-6 lg:px-8">
+                    <div className="md:flex md:items-center md:justify-center md:space-x-14 order-2">
+                        <div className="relative flex-shrink-0 w-48 h-48">
+                            <div className="absolute w-48 h-48 bg-gray-300 rounded-full -bottom-2 -right-1" />
+                            <img className="relative object-cover w-48 h-48 rounded-full" src={userInfo?.photoURL} alt="" />
+                        </div>
+
+                        <div className="mt-10 md:mt-0">
+                            <p className="text-6xl font-bold text-black mt-7">
+                                Welcome
+                                <br />
+                                {userInfo?.displayName}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
