d0ec6f74b1e78e39d5ea3a7451a22dafe711c71a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-17 21:58:59 +0600

    done the popoup system

--- assignment-task-ovigo/client/src/components/pages/SearchResultPages/SearchResult.jsx
+++ assignment-task-ovigo/client/src/components/pages/SearchResultPages/SearchResult.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-props-no-spreading */
 import moment from 'moment';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
 import SearchCard from './SearchCard';
@@ -10,6 +11,19 @@ const SearchResult = () => {
     const { state } = useLocation();
     const { register, handleSubmit } = useForm();
     const onSubmit = (data) => console.log(data);
+    const [showPopup, setShowPopup] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowPopup(true);
+        }, 5000);
+
+        return () => clearTimeout(timer); // Cleanup the timer when the component unmounts
+    }, []);
+
+    const closeModal = () => {
+        setShowPopup(false);
+    };
     return (
         <div className="h-[30vh] bg-blue-900 pt-40">
             <div className="grid grid-cols-3 justify-items-center my-32">
@@ -76,6 +90,24 @@ const SearchResult = () => {
                 </div>
                 <div className="col-span-2 flex flex-col gap-10 w-full p-10">{state[1]?.hotels && state[1]?.hotels.map((item) => <SearchCard key={item?.hotelId} data={item} />)}</div>
             </div>
+            {showPopup && (
+                <div className="fixed inset-0 flex items-center justify-center z-10">
+                    <div className="modal-overlay absolute inset-0 bg-black opacity-50" />
+                    <div className="modal-container bg-white w-1/2 md:w-96 mx-auto rounded-lg shadow-lg z-20">
+                        <div className="modal-content p-4">
+                            <h3 className="text-lg font-bold mb-4">Limited Time Offer!</h3>
+                            <p className="mb-4">
+                                Hurry up! Get 20% off on your first purchase. Use code <span className="text-orange-500 text-xl font-bold tracking-wider">FIRST20</span> at checkout.
+                            </p>
+                            <button type="button" htmlFor="close-modal" className="btn btn-primary" onClick={closeModal}>
+                                Please Booking First
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {/* Put this part before the </body> tag */}
+            <input type="checkbox" id="close-modal" className="modal-toggle" />
         </div>
     );
 };
