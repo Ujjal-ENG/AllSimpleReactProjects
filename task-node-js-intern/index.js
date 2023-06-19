b9a511f10868cf7640aae76b00cff4df2bdf81e0
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-19 21:20:53 +0600

    done the /api/users/ route create

--- task-node-js-intern/index.js
+++ task-node-js-intern/index.js
@@ -9,6 +9,14 @@ const app = express();
 // default middleware
 app.use(express.json());
 app.use(cors());
+
+// data
+const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' },
+    { id: 3, name: 'Bob Johnson' },
+];
+
 // default routes
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello from Node JS Intern Task Server!!' });
@@ -17,6 +25,20 @@ app.get('/', (req, res) => {
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'This project Health is Very Good!!!' });
 });
+
+// send all user
+app.get('/api/users', (req, res) => {
+    try {
+        res.status(200).json({
+            success: true,
+            message: 'Succssfully get the All Users',
+            data: users,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 // listen the app
 app.listen(PORT, () => {
     console.log('The Server is running on PORT 8080');
