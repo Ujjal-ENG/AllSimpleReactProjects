70c6b410345cb5304cd2b765ccb9c7abfe954420
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-20 17:02:36 +0600

    done the make search bar

--- e-com-assignment-task/src/components/pages/Home.jsx
+++ e-com-assignment-task/src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header/Header';
+import Searchbar from './SearchBar.jsx/Searchbar';
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Searchbar />
         </div>
     );
 };
--- e-com-assignment-task/src/components/pages/SearchBar.jsx/Searchbar.jsx
+++ e-com-assignment-task/src/components/pages/SearchBar.jsx/Searchbar.jsx
@@ -1,7 +1,25 @@
+/* eslint-disable react/jsx-indent-props */
 import React from 'react';
+import { BiSearchAlt2 } from 'react-icons/bi';
 
 const Searchbar = () => {
-    return <div>Searchbar</div>;
+    return (
+        <div className="max-w-[1400px] px-14 py-5 mx-auto bg-white  duration-150 transition-all ease-in-out hover:border-orange-400 hover:border-4 border-2 rounded-md  -mt-10 flex w-full justify-between items-center shadow-2xl">
+            <div className="mt-2.5 relative  w-full text-gray-400 focus-within:text-gray-600 max-w-5xl">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <BiSearchAlt2 size={24} />
+                </div>
+                <input
+                    placeholder="Up for some learning and growing today?"
+                    className="block font-bold w-full  py-4 pl-10  text-black placeholder-gray-500 transition-all duration-200 rounded-md bg-gray-50 focus:outline focus:border-blue-600 focus:bg-white caret-blue-600"
+                />
+            </div>
+
+            <button type="button" className="btn btn-ghost px-10 bg-gray-600 text-white text-2xl font-bold tracking-wider">
+                Search
+            </button>
+        </div>
+    );
 };
 
 export default Searchbar;
