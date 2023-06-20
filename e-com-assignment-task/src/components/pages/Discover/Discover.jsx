f28139618f9b078fd19f85cc6fb57b7e2dbe5001
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-20 19:35:42 +0600

    some changes in design

--- e-com-assignment-task/src/components/pages/Discover/Discover.jsx
+++ e-com-assignment-task/src/components/pages/Discover/Discover.jsx
@@ -5,7 +5,7 @@ const Discover = () => {
     return (
         <div>
             <h1 className="text-5xl tracking-wider text-center pb-4 font-bold">What Will You Discover?</h1>
-            <p className="text-xl font-semibold tracking-wider text-center">Students are viewing. What you find just might interest and inspire you. Tremendously.</p>
+            <p className="text-xl font-semibold tracking-wider text-center pb-8">Students are viewing. What you find just might interest and inspire you. Tremendously.</p>
             <div>
                 <CategorieItem />
             </div>
