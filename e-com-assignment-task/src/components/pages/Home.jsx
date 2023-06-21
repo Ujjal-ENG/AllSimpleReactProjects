8bbbdddd3c51b62f40b713b63ad2e00125373722
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-21 16:59:57 +0600

    popup message shown in after 5s for notice user attention

--- e-com-assignment-task/src/components/pages/Home.jsx
+++ e-com-assignment-task/src/components/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useEffect, useState } from 'react';
 import Categories from './Categories/Categories';
 import Company from './Company/Company';
 import Discover from './Discover/Discover';
@@ -12,6 +13,19 @@ import Testmonicals from './TestMonials/Testmonicals';
 import Testmonials2 from './TestMonials2/Testmonials2';
 
 const Home = () => {
+    const [showPopup, setShowPopup] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowPopup(true);
+        }, 5000);
+
+        return () => clearTimeout(timer); // Cleanup the timer when the component unmounts
+    }, []);
+
+    const closeModal = () => {
+        setShowPopup(false);
+    };
     return (
         <div>
             <Header />
@@ -25,6 +39,23 @@ const Home = () => {
             <Slider />
             <Testmonials2 />
             <Partners />
+            {showPopup && (
+                <div className="fixed inset-0 flex items-center justify-center z-10">
+                    <div className="modal-overlay absolute inset-0 bg-black opacity-50" />
+                    <div className="modal-container bg-white w-1/2 md:w-96 mx-auto rounded-lg shadow-lg z-20">
+                        <div className="modal-content p-4">
+                            <h3 className="text-lg font-bold mb-4">Limited Time Offer!</h3>
+                            <p className="mb-4">
+                                Hurry up! Get 20% off on your first purchase. Use code <span className="text-orange-500 text-xl font-bold tracking-wider">FIRST20</span> at checkout.
+                            </p>
+                            <button type="button" htmlFor="close-modal" className="btn btn-primary" onClick={closeModal}>
+                                Shop Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            <input type="checkbox" id="close-modal" className="modal-toggle" />
         </div>
     );
 };
