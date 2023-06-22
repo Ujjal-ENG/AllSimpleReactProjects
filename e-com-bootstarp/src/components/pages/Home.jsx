46ee3a425105d3066be8e21bdda587b7dfd5db02
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-22 21:56:11 +0600

    done the another setion

--- e-com-bootstarp/src/components/pages/Home.jsx
+++ e-com-bootstarp/src/components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Learn from './Learn/Learn';
+import LearnOwn from './OwnTime/LearnOwn';
 import Partners from './Partners/Partners';
 import Slider from './Slider/Slider';
 import Testmonials2 from './TestMonials2/Testmonials2';
@@ -7,6 +8,7 @@ import Testmonials2 from './TestMonials2/Testmonials2';
 const Home = () => {
     return (
         <div>
+            <LearnOwn />
             <Learn />
             <Slider />
             <Testmonials2 />
---
+++ e-com-bootstarp/src/components/pages/OwnTime/LearnOwn.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+import React from 'react';
+import './learnown.css';
+
+const LearnOwn = () => {
+    return (
+        <div>
+            <div className="container-md py-5">
+                <div className="row">
+                    <div className="col d-flex flex-column align-items-start justify-content-center">
+                        <h2 className="section-title1 py-2">Learn At Your Own Time</h2>
+                        <div className="bar1" />
+                        <p className="section-paragraph1 py-2">
+                            Everyone has their personal sweet spot when it comes to learning. With Uplyrn, you can choose to learn at any time, anywhere. But maybe not on a first date!
+                        </p>
+                    </div>
+                    <div className="col">
+                        <img src="https://uplyrn.com/images/webpimg/LearnAtYourOwnTime.webp" alt="avatar" className="section-image1 mx-5" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LearnOwn;
